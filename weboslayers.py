2c4dc42cd8e56bfb3a74bed7915eef05ed992584
Changhyeok Bae <changhyeok.bae@lge.com>
2018-07-13 11:45:53 +0900

    webos-image: Update layer pins for 2018-07-13 (build 9 2/2)
    meta-webosose as of 2018-07-12 19:42:27 -0700
    
    f8882e8 g-media-pipeline=1 gstreamer=1.14.0 umediaserver=2
            umediaserver-config=2 media-resource-calculator=2 avoutputd=2
    bf9b1f5 nodejs-module-webos-service=2 com.palm.service.devmode=2
    d730654 com.webos.app.settings=3

--- weboslayers.py
+++ weboslayers.py
@@ -75,7 +75,7 @@ webos_layers = [
 ('meta-webos-backports-2.5',  32, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 ('meta-webos-backports-2.6',  33, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=7f96238', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=f8882e8', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=morty,commit=2a19226', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
