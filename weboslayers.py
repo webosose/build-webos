d03b92d0d24fe0c6af3e39c5650e552652d74310
Eunmin Lim <eunmin.lim@lge.com>
2018-07-26 16:07:26 +0900

    weboslayers.py: add meta-ros2
    :Release Notes:
    Add meta-ros2, meta-webos-ros2 for ros2 packages
    
    :Detailed Notes:
    
    :QA Notes:
    
    :Issues Addressed:
    [PLAT-63815] [OSE] CCC: Add ros2 packages
    [PLAT-62985] Enable ros2 services on webOS OSE

--- weboslayers.py
+++ weboslayers.py
@@ -75,8 +75,11 @@ webos_layers = [
 ('meta-webos-backports-2.5',  32, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 ('meta-webos-backports-2.6',  33, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
+('meta-ros2',                 38, 'git://github.com/lgsvl/meta-ros2.git',                   'branch=ros2pr,commit=16805b2',''),
 ('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=94b3bff', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=morty,commit=2a19226', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
+
+('meta-webos-ros2',           52, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 ]
