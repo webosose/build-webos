50d4d6e66b092dca28fa5addbe31a1d88976e8c1
Minjae Kim <nate.kim@lge.com>
2018-09-20 15:33:56 +0900

    webos-image: Update layer pins for 2018-09-20 (build 20)
    meta-webosose as of 2018-09-20 15:30:00 +0900
    
    cc478cb com.webos.service.memorymanager=4
    b4d4720 com.webos.app.settings=4
    35c4657 webos_app.bbclass webos_app_generate_security_files.bbclass:
            improve manifest file generation for native apps.
    35f7000 sleepd=2-r7 cjson=0
    7cb6bab webos-fonts=2
    cba49ce com.webos.app.enactbrowser=7-r1
    7fab723 g-media-pipeline=4
    6335fd9 chromium53=9
    ec03c82 chromium53=8
    4c35cc0 libgoogleassistant=3 com.webos.service.ai=3

--- weboslayers.py
+++ weboslayers.py
@@ -76,7 +76,7 @@ webos_layers = [
 ('meta-webos-backports-2.6',  33, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
 ('meta-ros2',                 38, 'git://github.com/lgsvl/meta-ros2.git',                   'branch=ros2pr,commit=16805b2',''),
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=f8a9993', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=cc478cb', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=morty,commit=2a19226', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
