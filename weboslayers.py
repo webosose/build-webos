c1b28de32bcfeb7a871a4968da89c7c492ac1677
Minjae Kim <nate.kim@lge.com>
2018-10-31 20:39:11 +0900

    webos-image: Update layer pins for 2018-10-31 (build 35)
    meta-webosose as of 2018-10-31 20:40:00 +0900
    
    21931ca linux-raspberrypi=webosrpi18
            (update the patch to apply and extend it for raspberrypi3-64)
    ae7cfb9 notificationmgr=3
    3a1bc5c rdxd=2

--- weboslayers.py
+++ weboslayers.py
@@ -76,7 +76,7 @@ webos_layers = [
 ('meta-webos-backports-2.6',  33, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
 ('meta-ros2',                 38, 'git://github.com/lgsvl/meta-ros2.git',                   'branch=ros2pr,commit=b92ab96',''),
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=5a11b07', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=21931ca', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=morty,commit=2a19226', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
