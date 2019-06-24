6abf4d0030249dcf0c0c88c07a1ca49903a3e8d3
Minjae Kim <nate.kim@lge.com>
2019-06-24 11:09:36 +0900

    webos-image: Update layer pins for 2019-06-24 (build 80)
    meta-webosose as of 2019-06-24 11:10:00 +0900
    
    bed21c9 webos.inc: Move WEBOS_DISTRO_PRERELEASE, WEBOS_TARGET_MACHINE_IMPL,
            WEBOS_TARGET_MACHINE_VARIANT variables to bbclass
    afb90c7 webos-image, procps, bootd, crashd, db8, pmtrace: Fix metadata when DISTRO != webos
    68d3e42 qtbase, umediaserver: Fix parsing without webos as a DISTRO
    126da35 luna-prefs-data=r2 (fix parsing without webos as a DISTRO)
    bf3567b webos-recipe-blacklist-world.inc: blacklist geoip, geoip-perl, ntop

--- weboslayers.py
+++ weboslayers.py
@@ -76,10 +76,9 @@ webos_layers = [
 ('meta-webos-backports-2.6',  33, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
 ('meta-ros2',                 38, 'git://github.com/lgsvl/meta-ros2.git',                   'branch=ros2pr,commit=b92ab96',''),
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=a6b9992', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=bed21c9', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=morty,commit=2a19226', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
-
 ('meta-webos-ros2',           52, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 ]
