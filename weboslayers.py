a5b61cd056a827eb3f1bba5625ef37f56de443c8
Minjae Kim <nate.kim@lge.com>
2019-09-23 14:06:59 +0900

    webos-image: Update layer pins for 2019-09-23 (build 1)
    meta-webosose as of 2019-09-23 16:10 +0900
    
    3044ee62ad webos: webruntime_72=11 (rewrite git history)
    d9944a71ae webos: webruntime_68=69 webruntime_72=7 wam=13 chromium-v8=chromium72.7
    0c82c48196 webruntime_68=68 webruntime_72=6 chromium-v8=chromium72.6 wam=12
    b9356692b1 webruntime=r20 (Include proper debug info in dbg pkg)
    e6a6c98412 meta-webos: webruntime_68=67 webruntime_72=5 wam=11
    50f8c403e3 meta-webos-ros: remove meta-webos-ros layer from meta-webosose
    1d65b2ca13 Make chromium72 as preferred version for webruntime
    5a9fae2f2c Add webruntime_72=v72.0.3626.121,4 wam=r29 chromium-v8=chromium72.5
    80d2f57eb5 chromium68=66
    6ec9f2d1f0 webos_enactjs_app.bbclass: enact apps chromium 68 target
    f8aa982fbb chromium68=65 wam=10 v8=11
    
    Change-Id: I4f675d0d647f27c9538bc7f894e7366bf3a7e975

--- weboslayers.py
+++ weboslayers.py
@@ -75,7 +75,7 @@ webos_layers = [
 ('meta-webos-backports-2.8',  31, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
 ('meta-ros',                  38, 'git://github.com/ros/meta-ros.git',                      'branch=legacy,commit=f1a2de5a',''),
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=268ab37', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=@91.release,commit=3044ee6', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=thud,commit=89bdf97', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
