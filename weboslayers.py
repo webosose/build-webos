26e97d78c568f703d95f3dae040dce3231ccfb42
Minjae Kim <nate.kim@lge.com>
2019-10-21 19:27:47 +0900

    webos-image: Update layer pins for 2019-10-21 (build 112)
    meta-webosose as of 2019-10-21 19:24:28 +0900
    
    e7ac7c7 luna-surfacemanager=290 wam=15 sam=8

--- weboslayers.py
+++ weboslayers.py
@@ -73,7 +73,7 @@ webos_layers = [
 ('meta-webos-backports-2.7',  30, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 ('meta-webos-backports-2.8',  31, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=267249c', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=e7ac7c7', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=thud,commit=89bdf97', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
