2bb848487af7f73eaa1b165e7a3b2b221ef6866d
Minjae Kim <nate.kim@lge.com>
2019-10-21 18:58:38 +0900

    webos-image: Update layer pins for 2019-10-21 (build 107)
    meta-webosose as of 2019-10-21 18:54:03 +0900
    
    da05235 qtdeclarative=webos42
    1ba7908 com.webos.service.tts=6
    b475c2f webos: Add raspberrypi4 as supported machine
    f69d55f meta-webos-raspberrypi: Backport linux-raspberrypi v4.19.58
    07f5b85 meta-webos-raspberrypi: sync from warrior
    2bc8173 luna-surfacemanager=288-r45 maliit-framework-webos=87
            qml-webos-bridge=112 qml-webos-components=46
            qml-webos-framework=140-r27 qt-features-webos=42 qtwayland=133
            qtwayland-webos=42 webos-wayland-extensions=31

--- weboslayers.py
+++ weboslayers.py
@@ -73,7 +73,7 @@ webos_layers = [
 ('meta-webos-backports-2.7',  30, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 ('meta-webos-backports-2.8',  31, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=4112eba', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=da05235', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=thud,commit=89bdf97', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
