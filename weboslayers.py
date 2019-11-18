c6774016b215faababfd0c058d851ef4702bac7d
Minjae Kim <nate.kim@lge.com>
2019-11-18 19:55:21 +0900

    webos-image: Update layer pins for 2019-11-18 (build 137)
    meta-webosose as of 2019-11-18 19:53:14 +0900
    
    96713197 connman=webos10 webos-connman-adapter=9
    c04fe5d8 bluetooth-sil-bluez5=25
    89433f27 appinstalld2=3-r3 webos-initscripts=38

--- weboslayers.py
+++ weboslayers.py
@@ -75,7 +75,7 @@ webos_layers = [
 ('meta-webos-backports-2.7',  30, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 ('meta-webos-backports-2.8',  31, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=a18ef6a0', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=96713197', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=thud,commit=4e5be97', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
