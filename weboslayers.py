e86782233909cd9574247a716c66c3092fa7cb6e
Minjae Kim <nate.kim@lge.com>
2020-01-22 17:13:56 +0900

    webos-image: Update layer pins for 2020-01-22 (build 151)
    meta-webosose as of 2020-01-22 17:11:16 +0900
    
    5bb2a6488 sam=14 com.webos.service.memorymanager=15 bootd=9
    c73047379 auto-luna-surface-manager=9 luna-surfacemanager=305
            qtwayland=138
    601ca9cdf webos-qt-sdk=r0
            nativesdk-packagegroup-sdk-host.bbappend=webos1
            meta-environment.bbappend=webos1

--- weboslayers.py
+++ weboslayers.py
@@ -75,7 +75,7 @@ webos_layers = [
 ('meta-webos-backports-2.7',  30, 'ssh://gpro.lge.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-3.0',  31, 'ssh://gpro.lge.com/webosose/meta-webosose',              '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=e6301c01b', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=5bb2a6488', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=thud,commit=4e5be97', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
