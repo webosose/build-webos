9e9979530ec517c4a6a808f9bda159847a306cfb
Minjae Kim <nate.kim@lge.com>
2020-03-17 15:25:15 +0900

    webos-image: Update to Qt 5.12.6 (build 162 part 1/3)
    meta-lg-webos as of 2020-03-17 15:30:00 +0100
    
    8cd4934efd Upgrade qt5 to 5.12.6
    
    meta-qt5 as of 2019-11-20 13:49:56 +0100
    
    20331bd6 qt5: update to Qt 5.12.6

--- weboslayers.py
+++ weboslayers.py
@@ -70,13 +70,13 @@ webos_layers = [
 
 ('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=thud,commit=d61874d', ''),
 ('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=thud,commit=7685c7d', ''),
-('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=17187128', ''),
+('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=20331bd', ''),
 
 ('meta-webos-backports-2.7',  30, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 ('meta-webos-backports-3.0',  31, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 ('meta-webos-backports-3.1',  32, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=41218ec0', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=e21276f5', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=thud,commit=4e5be97', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
