cbc5f404f93ec3fa6c9ec1d96c4359f03e06f5f5
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2020-05-08 14:13:47 +0900

    scripts/build.sh: Fix webos-bom format (v6.10.7)
    :Release Notes:
    Fix webos-bom.json to right format
    
    :Detailed Notes:
    webos builds generate webos-bom.diff file as an json format but there is something to fix.
    
    as-is: {..}, {..}, ...{..},
    
    to-be: [{..}, {..}, ....{..}]
    
    :Testing Performed:
    - tested with local pc which have same enviroments with official
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [PLAT-102640] Fix webos-bom.json to right format

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2013-2019 LG Electronics, Inc.
+# Copyright (c) 2013-2020 LG Electronics, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@
 #set -x
 
 # Some constants
-SCRIPT_VERSION="6.10.6"
+SCRIPT_VERSION="6.10.7"
 SCRIPT_NAME=`basename $0`
 AUTHORITATIVE_OFFICIAL_BUILD_SITE="rpt"
 
@@ -207,10 +207,11 @@ function generate_webos_bom {
   F=$3
 
   rm -f webos-bom.json
+  rm -f webos-bom-sort.json
   /usr/bin/time -f "$TIME_STR" bitbake --runall=write_bom_data ${I} 2>&1 | tee /dev/stderr | grep '^TIME:' >> ${BUILD_TIME_LOG}
   [ -d ${ARTIFACTS}/${MACHINE}/${I} ] || mkdir -p ${ARTIFACTS}/${MACHINE}/${I}
-  sort webos-bom.json > ${ARTIFACTS}/${MACHINE}/${I}/${F}
-  rm -f webos-bom.json
+  sort webos-bom.json > webos-bom-sort.json
+  sed -e '1s/^{/[{/' -e '$s/,$/]/' webos-bom-sort.json > ${ARTIFACTS}/${MACHINE}/${I}/${F}
 }
 
 function filter_images {
