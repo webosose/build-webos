a5fb17f3a56c0f5488557d8251182ac19116d44b
Minjae Kim <nate.kim@lge.com>
2020-07-17 13:37:34 +0900

    webos-image: Update layer pins for 2020-07-17 (build 205)
    meta-webosose as of 2020-07-17 13:36:29 +0900
    
    2cfbcbc4 ose: webruntime_79=29 umediaserver-configs=9 test-apps=12
    3762561c ose: com.webos.app.enactbrowser=27
    58b8baad ose: freetype=webos1 fontconfig=webos1 webos-fonts=5

--- weboslayers.py
+++ weboslayers.py
@@ -76,7 +76,7 @@ webos_layers = [
 ('meta-webos-backports-3.0',  31, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 ('meta-webos-backports-3.1',  32, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=101ffdbd1e', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=2cfbcbc4', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=thud,commit=4e5be97', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
