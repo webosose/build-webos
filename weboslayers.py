fe24db9eef5e5fe80dbe1b1f36e06289c12dd16a
Minjae Kim <nate.kim@lge.com>
2020-09-26 12:34:54 +0900

    webos-image: Update to latest revision in Yocto 3.1 Dunfell (build 231)
    Yocto 3.1.1 Dunfell - July 2020:
    https://lists.yoctoproject.org/g/yocto/message/49668
    
    oe-core as of 2020-07-23 04:07:37 -1000
    bitbake as of 2020-07-23 16:50:51 +0100
    meta-oe as of 2020-07-01 08:01:41 -0700
    meta-raspberrypi as of 2020-07-23 13:06:29 +0100
    meta-updater as of 2020-07-14 09:42:23 +0200
    meta-virtualization as of 2020-07-06 16:29:00 -0400
    meta-security as of 2020-07-14 21:27:35 -0700
    meta-python2 as of 2020-04-27 13:30:34 -0700
    
    See the more details:  https://jira2.lgsvl.com/browse/PLAT-115145
    
    meta-webosose as of 2020-09-26 12:16:57 +0900
    
    118bb254 webos: layer.conf: Update to dunfell release name series
    bd821355 webos: downgrade bluez5 5.54 to 5.50
    dcca98f2 webos: downgrade nodejs 12.14.1 to 8.12.0
    e2728ba8 webos: Add com.webos.service.tts com.webos.service.ai
    991c3439 webos: downgrade grpc 1.24.3 to 1.14.1 and protobuf 3.11.4 to 3.6.1
    afb9ea2f webos: pulseaudio=r21
    de3955b9 webos: iotivity-node: remove python3 use
    2441bd30 webos.inc: temporarily drop Werror=return-type from zchunk
    30300df5 auto: docker-moby=webosvirt3: remove already created directory
    ed9d7fd2 webos: refresh patches to apply cleanly on bluez5 version 5.54
    7b513083 webos: wam: set disable neva media for use software codec
    c045044e webos: localization-tool-native: inherit pythonnative
    86b014ee userland: remove egl changes and 0002-Remove-EGL-dependency.patch
    af42c85f python3-pymisp: fix build-deps QA issue with bash
    d73184cc webos-recipe-blacklist.inc: add pipewire
    1923fe91 meta-qt5-compat: restore this layer for meta-qt5/warrior compatibility for dunfell
    8a68d6a9 console-tools: drop unnecessary .bbappend
    189459e3 bluez5: update patches to apply on 5.54 version
    9de1e9b6 xen-tools, babeltrace2: bash QA
    e1d12769 busybox: refresh patches to apply cleanly on 1.31.1
    f927c858 wayland: update the patch files to apply on 1.18.0 version
    e5b3de4a nss, nspr, mozjs: remove upstreamed changes for nspr includedir
    3d12b5da nss: refresh do_compile from upstream recipe
    4acd4eee rpi: mesa: update 0001-implement-wl-drm-create-linear-prime-buffer.patch to apply on 20.0.1 version
    f80dcc3b iotop: update .patch to apply on new SRCREV
    8e383942 userland: remove changes which are now in upstream
    69e454cb leveldb-tl: fix maybe-uninitialized error
    24701f18 settingsservice,iotivity-node: remove all python2 dependencies
    bf38e744 umediaserver: remove test/python until migrated to python3
    c06d71f2 pmtrace: use python3
    2c08f2a6 nodejs-module-node-red: use python3
    ade87eb3 python3, ifplugd, babeltrace, pulseaudio, gensio: fix bash runtime dependency
    927418fe crda=0
    68f2b957 nodejs-module-webos-{dynaload,pmlog,sysbus}: use python3 for builds
    33696846 node-gyp-native: upgrade to 6.1.0 to support python3
    afd5dac1 ffmpeg: drop 2.3.6 version and use 4.2.1 from oe-core
    5036d725 gstreamer: adapt to oe-core recipes using meson now
    acba098b gettext: fix QA about runtime dependencies on locale-base-*
    1516d0f3 u-boot-fw-utils: drop bbappend
    f3aa393b cpio: drop unnecessary .bbappend
    a4da4616 iotivity-node: inherit pythonnative and use python(2) for now
    df3972a9 nodejs-module-webos-sysbus: inherit pythonnative and use python(2) for now
    80a8cdd4 nodejs-module-webos-{pmlog, dynaload}: inherit pythonnative and use python(2) for now
    aa646540 bluez5: update patches to apply on 5.52 version
    09319bda sed: rename bbappend to match new version
    cac1d1ec leveldb: rename bbappend to match new version
    6a0276da u-boot: update patch to apply on 2020.01~rc3 version
    b3318520 pulseaudio: return dependency on intltool-native
    0350e400 webos: PNBLACKLIST: update for Yocto 3.1 Dunfell
    6a5a5a2f meta-webos-backports: drop 3.1

--- weboslayers.py
+++ weboslayers.py
@@ -58,31 +58,31 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.44,commit=fd279f85', ''),
+('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.46,commit=cc11dfa4', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=zeus,commit=f5b90eeed7', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=ea886d57db', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=zeus,commit=2b5dd1eb81', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=dunfell,commit=cc6fc6b164', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
-('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=zeus,commit=2152ac8', ''),
-('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=zeus,commit=514ce5f', ''),
+('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=dunfell,commit=4af1ea6', ''),
+('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=dunfell,commit=ffd787f', ''),
+('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=dunfell,commit=e2ef0dd', ''),
 
+('meta-qt5-compat',           19, 'git://github.com/webosose/meta-webosose',                '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=7d0b17a', ''),
 
-('meta-webos-backports-3.1',  31, 'git://github.com/webosose/meta-webosose.git',            '', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=118bb254', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=9edc6a6e', ''),
-
-('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=zeus,commit=0e05098', ''),
+('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=eb2f6d4', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
 ('meta-webos-updater',        52, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 ('meta-webos-virtualization', 53, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
 ('meta-webos-smack',          75, 'git://github.com/webosose/meta-webosose.git',            '', ''),
-('meta-security',             77, 'git://git.yoctoproject.org/meta-security',               'branch=zeus,commit=440c37f', ''),
+('meta-security',             77, 'git://git.yoctoproject.org/meta-security',               'branch=dunfell,commit=982a29b', ''),
 ]
