85b7b3d24ba9bf2f12cbcc0c8b846d2670f98a63
Minjae Kim <nate.kim@lge.com>
2020-09-26 13:21:49 +0900

    webos-image: Update layer pins for 2020-09-26 (build 241)
    meta-webosose as of 2020-09-26 13:20:52 +0900
    
    b9c351b8 com.webos.service.tts=12
    e882435b com.webos.service.power2=3
    975cfd57 nodejs-module-webos-pmlog=5 nodejs-module-webos-sysbus=6
    65b976de com.webos.service.bluetooth2=40 bluetooth-sil-bluez5=43
             bluetooth-sil-api=18
    fc25a50f com.webos.service.hfp=16
    78b6907c luna-surfacemanager=330

--- weboslayers.py
+++ weboslayers.py
@@ -75,7 +75,7 @@ webos_layers = [
 ('meta-qt5-compat',           19, 'git://github.com/webosose/meta-webosose',                '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=7d0b17a', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=c012def0', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=b9c351b8', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=bc7a066', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
