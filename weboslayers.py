799d01e0d705cd7463be6d2d3d5e04908838f547
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2020-09-26 11:09:33 +0900

    webos-image: Update layer pins for 2020-09-26 (build 228)
    meta-webosose as of 2020-09-26 11:07:12 +0900
    
    ebfddde9 qtbase=webos79webosrpi12 qtdeclarative=webos53
             webos-initscripts=r15
    287aeb43 bluetooth-sil-bluez5=34 com.webos.service.bluetooth2=31
    669dca58 com.webos.service.pdm=12
    517d7c95 settingservice-conf=6

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.0',  31, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 ('meta-webos-backports-3.1',  32, 'git://github.com/webosose/meta-webosose.git',            '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=633d3802', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=ebfddde9', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=thud,commit=4e5be97', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
