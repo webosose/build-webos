cee35128575d90766ed98641321b385c7c3ff448
Martin Jansa <martin.jansa@lge.com>
2020-09-26 13:40:28 +0900

    webos-image: Update to Qt 5.12.9 (build 243 part 2/3)
    meta-webosose as of 2020-08-31 14:59:00 +0900
    
    0b9498a9 qtbase=webos80 qtdeclarative=webos56 (Update for Qt 5.12.9)
    
    meta-qt5 as of 2020-07-22 14:08:11 +0200
    
    9248dc7 qtbase-native: disable vulkan support
    7565dbe qtbase: Add vulkan to PACKAGECONFIGs
    90f99d8 qt5: upgrade to 5.12.9

--- weboslayers.py
+++ weboslayers.py
@@ -73,9 +73,9 @@ webos_layers = [
 ('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=dunfell,commit=e2ef0dd', ''),
 
 ('meta-qt5-compat',           19, 'git://github.com/webosose/meta-webosose',                '', ''),
-('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=7d0b17a', ''),
+('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=9248dc7', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=a8d9dff9', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=0b9498a9', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=93305f7', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
