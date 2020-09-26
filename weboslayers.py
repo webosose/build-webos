a00b3a8a74e2d9169a3fb09d9cc46c10b72104c3
Martin Jansa <martin.jansa@lge.com>
2020-09-26 14:00:34 +0900

    webos-image: Update to latest revision in Yocto 3.1 Dunfell
    oe-core as of 2020-09-04 04:31:45 -1000
    
    210ebed1e9 package: get_package_mapping: avoid dependency mapping if
            renamed package provides original name
    83b7445878 linux-libc-headers: kernel headers are installed in
            STAGING_KERNEL_BUILDDIR
    09bc151f57 json-glib: Backport a build fix with clang
    86eb281069 selftest/prservice: Improve test failure message
    85b1e83557 devtool: deploy-target: Fix size calculation for hard links
    60b474320e insane: improve gnu-hash-style warning
    af265eca8c insane: fix gnu-hash-style check
    cf6cab12ca oeqa: sdk: Capture stderr output
    497d883c0f util-linux: Allow update alternatives for additional apps
    ecfc52630f recipes-kernel: linux-firmware add qcom-venus-{5.2,5.4}
            packages
    2427c872ba wic: misc: Add /bin to the list of searchpaths
    89cb403fce sysstat: fix installed-vs-shipped QA Issue in systemd
    5d8b81fd0f sanity.conf: update BB_MIN_VERSION to 1.46.0
    b0fb7f18f9 cml1: Move find_cfgs() helper to cml1.bbclass
    d26c5882ee json-c: Fix CVE-2020-12762
    e397859cea kernel-yocto: checksum all modifications to available
            kernel fragments directories
    2de84e6e76 devtool: expand SRC_URI when guessing recipe update mode
    be88ca5c4b package.bbclass: explode the RPROVIDES so we don't think
            the versions are provides
    
    meta-oe as of 2020-09-03 08:28:37 -0700
    
    d7b41ced4b net-snmp: Fix CVE-2020-15861 and CVE-2020-15862
    b72b233d26 gnome-settings-daemon: Remove duplicate outdated SRC_URI
            hashes
    a6adc08626 gnome-settings-daemon: Backport 3.36 fix for building
            without wayland
    6a8d6ddc54 babl-native: fix build issue
    eec787e19b vlc: fix loop initial declarations are only allowed in
            C99 mode
    fd16ab3d64 python3-pandas: Upgrade 1.0.3 -> 1.0.5
    0bc6614b1a lcov: fix lcov-native build
    5aa6849a90 freeradius: fix the occasional verification failure
    57b629543e rdist: fix parallel build
    060d218f82 samba: upgrade 4.10.15 -> 4.10.17
    5fe92ba89e exiv2: upgrade 0.27.1 -> 0.27.3
    fae2b90261 lua: Security Advisory - lua - CVE-2020-15888
    40f4f3ed5f flashrom: Fix build failure with glibc 2.32
    e227d95f2f samba: Fix conflicts with nss.h from glibc
    f1dc6e6f5d python3-pint: add setuptools and packaging to RDEPENDS
    135307cf51 python3-obd: Add missing setuptools RDEPENDS
    022b5a307c radvd: add /etc/radvd.conf
    b4be735fdb netkit-telnetd: Fix buffer overflow in netoprintf
    fd0d398fe7 freeradius: fix the existed certificate error
    1e19fc4d9c lvm2: remove service template from SYSTEMD_SERVICE
    
    meta-updater as of 2020-09-07 16:02:25 +0300
    
    a491e47 Merge pull request #773 from
            liuming50/introduce-OSTREE_MULTI_DEVICETREE_SUPPORT-for-dunfell
    bd0a055 sota: introduce OSTREE_MULTI_DEVICETREE_SUPPORT
    
    meta-python2 as of 2020-09-11 16:22:04 -0700
    
    07dca1e kas-meta-python-ptest-image: fixes for GL
    bf7424e python: prevent lib32-python depending on python

--- weboslayers.py
+++ weboslayers.py
@@ -60,17 +60,17 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 webos_layers = [
 ('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.46,commit=838a8914', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=0d4d0df608', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=210ebed1e9', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=dunfell,commit=654ad8bea4', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=dunfell,commit=d7b41ced4b', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
-('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=dunfell,commit=805e2c6', ''),
+('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=dunfell,commit=a491e47', ''),
 ('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=dunfell,commit=ff997b6', ''),
-('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=dunfell,commit=e2ef0dd', ''),
+('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=dunfell,commit=07dca1e', ''),
 
 ('meta-qt5-compat',           19, 'git://github.com/webosose/meta-webosose',                '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=9248dc7', ''),
