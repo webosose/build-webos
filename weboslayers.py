58c67226e2482e1a9ba4ff37190630823b0e9b89
hyunjae92.shin <hyunjae92.shin@lgepartner.com>
2020-11-10 10:45:00 +0900

    webos-image: Update layer pins for 2020-11-09 (build 251)
    meta-webosose as of 2020-11-09 15:07:06 +0900
    
    ca682c8628 luna-surfacemanager=334 qml-webos-bridge=120
               qtwayland=158 qtwayland-webos=57
    c64151dab1 ose: com.palm.service.devmode=6 sam=50
    561febb61c ose: webruntime_79=46
    8b504932dc notificationmgr=14
    8e33e675c7 com.webos.service.tts=13
    2f04198ae4 com.webos.service.pdm=25
    fdcd8ae7cf ose: com.webos.service.mediacontroller=15

--- weboslayers.py
+++ weboslayers.py
@@ -75,7 +75,7 @@ webos_layers = [
 ('meta-qt5-compat',           19, 'git://github.com/webosose/meta-webosose',                '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=9248dc7', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=50794c35', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=ca682c8628', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=93305f7', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
