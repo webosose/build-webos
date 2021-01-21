59b01ff048213ac6f624cabbf7a310f51fba702d
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-01-21 16:54:41 +0900

    webos-image: Update layer pins for 2021-01-21 (build 285)
    meta-webosose as of 2021-01-21 16:53:50 +0900
    
    674cc240f ose: bluetooth-sil-bluez5=49 com.webos.service.bluetooth2=46
              com.webos.service.hfp=22
    da62abea6 ose: com.webos.service.location=85 nyx-modules=15

--- weboslayers.py
+++ weboslayers.py
@@ -75,7 +75,7 @@ webos_layers = [
 ('meta-qt5-compat',           19, 'git://github.com/webosose/meta-webosose',                '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=6a9de2d', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=8c6b0cbe4', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=674cc240f', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=9879932', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
