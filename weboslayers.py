f10b21b531b2c1d14d2af7a6411144720c72f1ad
Minjae Kim <nate.kim@lge.com>
2021-03-30 14:41:09 +0900

    webos-image: Update layer pins for 2021-03-30 (build 298)
    meta-webosose as of 2021-03-30 14:38:01 +0900
    
    73dacc57 nodejs-module-usocket=r4 nodejs-module-webos-pmlog=6-r14
            nodejs-module-webos-sysbus=7
    9eb1a9d0 ose: event-monitor=8
    8e5a23b5 com.webos.service.mediacontroller=20 umediaserver=24.gav.15
            g-media-pipeline=21.gav.18
    c02a1262 ose: webos-connman-adapter=27
    ffb2925d ose: g-camera-pipeline=13.gav.11 com.webos.service.camera=16
    9c18f69c luna-prefs=8
    1708b021 tzdata=webos7 libwebosi18n=5
    b02a33e5 luna-surfacemanager=345 qml-webos-bridge=122
            qml-webos-components=51 qt-features-webos=49 qtbase-plugins-webos=2
            qtwayland=163 qtwayland-webos=61

--- weboslayers.py
+++ weboslayers.py
@@ -75,7 +75,7 @@ webos_layers = [
 ('meta-qt5-compat',           19, 'git://github.com/webosose/meta-webosose',                '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=6a9de2d', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=afa395c8', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=73dacc57', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=9879932', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
