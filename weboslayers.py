ff4a947593d5a010dd6f2acc96e0c98b1605b816
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-04-01 09:43:07 +0900

    webos-image: Update layer pins for 2021-04-01 (build 323)
    meta-webosose as of 2021-04-01 09:41:54 +0900
    
    93f98cc7b com.webos.app.mediagallery=6
    e82f8bcf7 bootchart2=r11
    69e6acd80 com.webos.app.videoplayer=5
    9af06daeb ose: luna-service2=14

--- weboslayers.py
+++ weboslayers.py
@@ -75,7 +75,7 @@ webos_layers = [
 ('meta-qt5-compat',           19, 'git://github.com/webosose/meta-webosose',                '', ''),
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=warrior,commit=c7ec556', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=4d0b0b04e', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=93f98cc7b', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=77190af', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
