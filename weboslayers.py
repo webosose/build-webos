bf842af6125db085e0557f5bc69b47417124be32
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-06-21 15:25:23 +0900

    webos-image: Update layer pins for 2021-06-21 (build 326)
    meta-webosose as of 2021-06-21 15:18:21 +0900
    
    6add31b35 com.webos.app.enactbrowser=44
    228758b8e webos-recipe-blacklist.inc: blacklist python(2)
    d4cae1a89 guider=v3.9.7

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 
 ('meta-webos-backports-3.2',  33, 'git://github.com/webosose/meta-webosose',                '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=7436eb6aa', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=6add31b35', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=77190af', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
