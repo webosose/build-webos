ceb2d8687e07c114c0c24e4beff5875834f300ec
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-06-22 10:30:45 +0900

    webos-image: Update layer pins for 2021-06-22 (build 335)
    meta-webosose as of 2021-06-22 10:29:51 +0900
    
    01002c387 luna-surfacemanager=351

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 
 ('meta-webos-backports-3.2',  33, 'git://github.com/webosose/meta-webosose',                '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=1dfe8e6dd', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=01002c387', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=77190af', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
