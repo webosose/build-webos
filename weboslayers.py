2b4af26ef5a87508eabf9db4b12786317547e940
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-06-22 15:14:55 +0900

    webos-image: Update layer pins for 2021-06-22 (build 352 part 3/3)
    meta-webosose as of 2021-06-22 15:07:52 +0900
    
    0aaae6a08 com.webos.app.settings=14
    cb9124367 ose: imemanager=22
    8b45009a0 com.webos.service.pdm=60
    75480cd2c com.webos.app.imageviewer=5
    acc423683 ilib-qml-plugin=6
    1e3e22831 ose:webos-fluentbit-plugins=9

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 
 ('meta-webos-backports-3.2',  33, 'git://github.com/webosose/meta-webosose',                '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=530be3749', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=0aaae6a08', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=77190af', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
