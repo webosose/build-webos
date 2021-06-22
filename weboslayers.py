d879b990df21f5fa435c0338a6eecb2b37af6bd3
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-06-22 16:17:01 +0900

    webos-image: Update layer pins for 2021-06-22 (build 356)
    meta-webosose as of 2021-06-22 16:16:07 +0900
    
    f1735e84c ose: com.webos.service.storageaccess=13

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 
 ('meta-webos-backports-3.2',  33, 'git://github.com/webosose/meta-webosose',                '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=32f9d20d6', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=f1735e84c', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=77190af', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
