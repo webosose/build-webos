988aa45226c5e2886696067a45404e34f2e841b5
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-06-22 10:44:37 +0900

    webos-image: Update layer pins for 2021-06-22 (build 342)
    meta-webosose as of 2021-06-22 10:43:50 +0900
    
    82750b25d ose: com.webos.service.storageaccess=9-r2 Add libgdrive=r0
    5787211c3 ose: g-camera-pipeline=13.gav.16 umediaserver=24.gav.18
    7f2756d71 com.webos.service.pdm=58
    84aa062c0 com.webos.app.imageviewer=3
    8681eb9c1 ose: webos-fluentbit-plugins=7
    559498d88 native: localization-tool-native=v1.4.0

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 
 ('meta-webos-backports-3.2',  33, 'git://github.com/webosose/meta-webosose',                '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=3b50d758f', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=82750b25d', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=77190af', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
