7fe0293c8237eb7c010059ad342a73a18c28addd
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-08-05 10:01:56 +0900

    webos-image: Update layer pins for 2021-08-05 (build 369)
    meta-webosose as of 2021-08-05 09:55:57 +0900
    
    37d6f271c ose: audiod=32
    5360a79c9 com.webos.service.bluetooth2=63
    4e3aff645 ose: Add webruntime_87=6-r48.0,v87.0.4280.88 wam=r39 Add
            mksnapshot-cross_87=r0
    f614519a0 Revert "webruntime-common.inc: Add path to find .inc file
            and remove duplicate"
    1a5a0e25b ose: systemd=webos6 webos-fluentbit-plugins=12

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.2',  33, 'git://github.com/webosose/meta-webosose',                '', ''),
 ('meta-webos-backports-3.4',  35, 'git://github.com/webosose/meta-webosose',                '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=07d90957b', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=37d6f271c', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=f0c7501', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
