74c110f99b01084b88a2b359a9b5964a2a2b4c00
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-08-05 10:00:54 +0900

    weboslayers.py: Add meta-webos-backport-3.4 (build 369 part 1/2)
    meta-webosose as of 2021-08-05 10:00:34 +0900
    
    07d90957b systemd=v248.3 add image-uefi.conf (backport newer version from Honister)
    629441b10 Add meta-webos-backports-3.4 from yocto 3.4 Honister
    
    :Detailed Notes:
    Add new layer for backports from Yocto 3.4 Honister
    It's currently empty, but will be used for backporting newer systemd.
    
    :Issues Addressed:
    [WRN-2613] CCC: systemd=v248.3
    [WRN-2338] Upgrade systemd v248.3 from Yocto 3.4 Honister for webOS

--- weboslayers.py
+++ weboslayers.py
@@ -75,8 +75,9 @@ webos_layers = [
 ('meta-qt6',                  20, 'git://code.qt.io/yocto/meta-qt6.git',                    'branch=6.2,commit=9a4453a', ''),
 
 ('meta-webos-backports-3.2',  33, 'git://github.com/webosose/meta-webosose',                '', ''),
+('meta-webos-backports-3.4',  35, 'git://github.com/webosose/meta-webosose',                '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=3791a6163', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=07d90957b', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=f0c7501', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
