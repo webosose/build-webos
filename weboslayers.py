7727eeaa19794ac96694342292593aa49a3cd790
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-09-08 18:19:30 +0900

    webos-image: Update layer pins for 2021-09-08 (build 381)
    meta-webosose as of 2021-09-08 17:53:30 +0900
    
    f71f9b017 com.webos.service.bluetooth2=65 bluetooth-sil-bluez5=66

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.2',  33, 'git://github.com/webosose/meta-webosose',                '', ''),
 ('meta-webos-backports-3.4',  35, 'git://github.com/webosose/meta-webosose',                '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=9c45e43b0', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=f71f9b017', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=59c2d6f', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
