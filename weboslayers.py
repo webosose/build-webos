7000ef2eaa87bae5e2b144d5abbbf135f3bc3584
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-09-13 15:01:44 +0900

    webos-image: Update layer pins for 2021-09-13 (build 391)
    meta-webosose as of 2021-09-13 15:00:26 +0900
    
    30821387 ose: wam=53-r40
    909730c6 glibc=webos7 base-passwd=webos6 cmake-modules-webos-native=r3
             Remove webos_crashddir in webos_filesystem_paths.bbclass

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.2',  33, 'git://github.com/webosose/meta-webosose',                '', ''),
 ('meta-webos-backports-3.4',  35, 'git://github.com/webosose/meta-webosose',                '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=87326b49', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=30821387', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=59c2d6f', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
