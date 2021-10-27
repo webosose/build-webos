27a6c6de3a5f6e5b2fe5f20949eaa3cd559b175f
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-10-27 15:58:30 +0900

    webos-image: Update layer pins for 2021-10-27 (build 398)
    meta-webosose as of 2021-10-27 16:00:51 +0900
    
    4714046a ose: enact-sandstone=r6
    b6378bd7 ose: com.webos.service.pdm=64
    d6e2a74d webos-fluentbit-plugins=21
    518d3a65 webos-recipe-blacklist.inc: blacklist sysdig due to our jsoncpp bbappend
    f3ecdebc com.webos.app.test.smack.native=r3 (inherit webos_pkgconfig)
    6879889f com.webos.service.tts=r5 (inherit webos_pkgconfig)
    a7d61e89 com.webos.service.ai=r5 (inherit webos_pkgconfig)
    f13001cf bluetooth-sil-bluez5=r4 (inherit webos_pkgconfig)
    8a34e25f qml-webos-bridge=r18 (inherit webos_pkgconfig)
    5ce3a7db qml-webos-framework=r34 (inherit webos_pkgconfig)
    f6fe9fd6 qml-app-components=r3 (inherit webos_pkgconfig)
    fbc3a36f qml-webos-components=r18 (inherit webos_pkgconfig)
    53d8c823 qtwayland-webos=r15 (inherit webos_pkgconfig)
    6f9b273c luna-surfacemanager=r52 (inherit webos_pkgconfig)
    a5d6441c bluetooth-sil-api=r4 (inherit webos_pkgconfig)
    107d36e4 loc-utils=r4 (inherit webos_pkgconfig)
    e7d41934 com.webos.service.mediaindexer=r7 (inherit webos_pkgconfig)
    fc2d72df com.webos.service.uwb=r2 (inherit webos_pkgconfig)
    e5a2ffa7 com.webos.service.unifiedsearch=r1 (inherit webos_pkgconfig)
    aa717f47 com.webos.service.storageaccess=r5 (inherit webos_pkgconfig)
    9e7b7b10 event-monitor-network=r2 (inherit webos_pkgconfig)
    a884cfea mksnaphost-cross=r2 (inherit pkgconfig)
    8b943452 wam=r42 (inherit webos_pkgconfig)
    840188b8 media-resource-calculator=r6 (inherit webos_pkgconfig)
    741c9050 webos-wayland-extensions=r6 (inherit webos_pkgconfig)
    7ed0c613 serviceinstaller=r5 (inherit webos_pkgconfig)
    ee6c5e51 webruntime=r50.* (inherit pkgconfig)
    b0e13bf8 webruntime: remove version 84
    35ce36cd webos_configure_manifest.bbclass: fix regexp DeprecationWarnings
    375f1bbe systemd=smack3 (update patch to apply on 248.3)
    a3680e11 systemd=smack2 (fix build with smack enabled)
    39840f43 wam=r41 (fix build with gcc-11)
    2400f73c lxc=webosvirt3 (fix runtime dependency on tar)
    bd8c6f62 db8=r35 (update for glibc-2.34)
    0831370d webruntime: don't include exportbuildscript task in dependency tree

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.2',  33, 'git://github.com/webosose/meta-webosose',                '', ''),
 ('meta-webos-backports-3.4',  35, 'git://github.com/webosose/meta-webosose',                '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=cdb0df7f0', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=4714046a', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=59c2d6f', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
