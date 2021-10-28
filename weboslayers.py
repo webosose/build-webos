d16c250a2ee44e9a6edaec88e6ff8b6ffc167359
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-10-28 13:47:56 +0900

    webos-image: Update to latest revision in Yocto 3.1 Dunfell (build 402 part 1/2)
    meta-webosose as of 2021-10-28 13:48:39 +0900
    
    3ee61b21 com.webos.service.flowmanager=r7 (fix build with openssl-3)
    89b92f0b rpm=webos4 (remove unsupported options)
    
    oe-core as of 2021-10-25 10:23:54 +0100
    
    62cdc20a21 uninative: Upgrade to 3.4
    5fc4f7896f target/ssh.py: add HostKeyAlgorithms option to test commands
    917f800368 python3: Add a fix for a make install race
    8fc9963d70 libnewt: Use python3targetconfig to fix reproducibility
            issue
    e570efa43d libxml2: Use python3targetconfig to fix reproducibility
            issue
    e30c2be8b9 externalsrc: Fix a source date epoch race in reproducible
            builds
    badef50c6c externalsrc: Work with reproducible_build
    419205da83 gobject-introspection: Don't write $HOME into scripts
    f1cc4b8d75 libtool: Allow libtool-cross to reproduce
    3dcc84e37c libtool: Fix lto option passing for reproducible builds
    c6a5c4a72d util-linux: Fix reproducibility
    a8ec8c9eae gnupg: Be deterministic about sendmail
    422ce20c7d mesa: Ensure megadrivers runtime mappings are deterministic
    00615c9edb package: Ensure pclist files are deterministic and don't
            use full paths
    03b4b26ced uninative: Upgrade to 3.3, support glibc 2.34
    effa24ebf2 uninative: Improve glob to handle glibc 2.34
    b7d269c848 nativesdk-pseudo: Fix to work with glibc 2.34 systems
    63afcafef7 pseudo: Update with fcntl and glibc 2.34 fixes
    449bb53b3e pseudo: Fix to work with glibc 2.34 systems
    7c2de7211d util-linux: disable raw
    eaebf0884d gpgme: Use glibc provided closefrom API when available
    6417148072 m4: Do not use SIGSTKSZ
    5690d18bb6 gcc: fix missing dependencies for selftests
    c912cd493f libpsl: Add config knobs for runtime/builtin conversion
            choices
    af46b360b3 patch.bbclass: when the patch fails show more info on the
            fatal error
    043214d288 oeqa/selftest/sstatetests: fix typo ware -> were
    4d0ad4962b tar: filter CVEs using vendor name
    006b5221ed rng-tools: add systemd-udev-settle wants to service
    a21d3d8f6d scriptutils.py: Add check before deleting path
    a4983b9878 binutils: Fix a missing break in case statement
    8809209c95 oeqa/manual: Fix no longer valid URLs
    771c7fe5ff multilib: Avoid sysroot race issues when multilib enabled
    4efdcc1090 weston: Use systemd notify,
    d4ec280230 e2fsprogs: upgrade 1.45.6 -> 1.45.7
    53947537ed e2fsprogs: update to 1.45.6
    09398a96fa linux-yocto/5.4: update to v5.4.153
    79a17b6c32 linux-yocto/5.4: update to v5.4.150
    79c4485069 linux-yocto/5.4: update to v5.4.149
    89df45b9e6 ffmpeg: Add fix for CVEs
    
    bitbake as of 2021-10-23 17:47:26 +0100
    
    c5b8a2fc fetch2/git: Use os.rename instead of mv
    e3da0ecb fetch2/git: Avoid races over mirror tarball creation
    74a1e71b hashserv: let asyncio discover the running loop
    1c422329 bitbake: correct deprecation warning in process.py
    e0d6af2f bitbake: adjust parser error check for python 3.10
            compatibility
    2dca6a22 bitbake: do not import imp in layerindexlib
    d5ac4af7 bitbake: fix regexp deprecation warnings
    4b66d498 bitbake: correct the collections vs collections.abc
            deprecation
    2a40a776 compat.py: remove file since it no longer actually implements
            anything
    
    meta-oe as of 2021-10-14 13:57:41 -0700
    
    814eec96c2 gattlib: Place pkgconfig file in correct package
    c1913c367f gattlib: remove includedir from base package
    
    meta-virtualization as of 2021-10-18 14:19:05 -0400
    
    b556bc7 busybox: Add nsenter for podman runtime
    
    :Issues Addressed:
    [WRN-9222] CCC: Upgrade to latest revisions from Yocto 3.1 Dunfell

--- weboslayers.py
+++ weboslayers.py
@@ -58,18 +58,18 @@ Machines = ['qemux86', 'raspberrypi3', 'raspberrypi4']
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.46,commit=b4117231', ''),
+('bitbake',                   -1, 'git://github.com/openembedded/bitbake.git',              'branch=1.46,commit=c5b8a2fc', ''),
 
-('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=d12c44df66', 'oe-core'),
+('meta',                       5, 'git://github.com/openembedded/openembedded-core.git',    'branch=dunfell,commit=62cdc20a21', 'oe-core'),
 
-('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=dunfell,commit=3cf22d1588', 'meta-oe'),
+('meta-oe',                   10, 'git://github.com/openembedded/meta-openembedded.git',    'branch=dunfell,commit=814eec96c2', 'meta-oe'),
 ('meta-multimedia',           11, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-networking',           12, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-python',               13, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 ('meta-filesystems',          14, 'git://github.com/openembedded/meta-openembedded.git',    '', 'meta-oe'),
 
 ('meta-updater',              15, 'git://github.com/advancedtelematic/meta-updater.git',    'branch=dunfell,commit=5d49b28', ''),
-('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=dunfell,commit=9e9868e', ''),
+('meta-virtualization',       16, 'git://git.yoctoproject.org/meta-virtualization',         'branch=dunfell,commit=b556bc7', ''),
 ('meta-python2',              17, 'git://git.openembedded.org/meta-python2',                'branch=dunfell,commit=b901080', ''),
 
 ('meta-qt6',                  20, 'git://code.qt.io/yocto/meta-qt6.git',                    'branch=6.2,commit=caaf2ea', ''),
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.2',  33, 'git://github.com/webosose/meta-webosose',                '', ''),
 ('meta-webos-backports-3.4',  35, 'git://github.com/webosose/meta-webosose',                '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=7168ee47', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=3ee61b21', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=59c2d6f', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
