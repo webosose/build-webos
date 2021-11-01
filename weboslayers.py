8c25fa1ce8973794df059be12e57ee76d3aedca1
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-11-01 13:43:57 +0900

    webos-image: Update layer pins for 2021-11-01 (build 403)
    meta-webosose as of 2021-11-01 13:47:58 +0900
    
    51e3250b ose: webruntime_87=10
    a4aee808 ose: Add com.webos.app.camera=8
    ea1c5901 bluetooth-sil-bluez5=76
    ffa515f7 ose: com.webos.app.home=18
    2f29443b ose: com.webos.service.pdm=68

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.2',  33, 'git://github.com/webosose/meta-webosose',                '', ''),
 ('meta-webos-backports-3.4',  35, 'git://github.com/webosose/meta-webosose',                '', ''),
 
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=3ee61b21', ''),
+('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=51e3250b', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=dunfell,commit=59c2d6f', ''),
 ('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
