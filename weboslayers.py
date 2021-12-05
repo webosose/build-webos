8ecca582d0cc2a1304ea37195cadefa6240863ff
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2021-12-06 08:40:46 +0900

    webos-image: Update layer pins for 2021-12-06 (build 416)
    meta-webosose as of 2021-12-06 08:41:13 +0900
    
    a3697874f ose: audiod=34
    b247c5f08 org.ocf.webossample.{occlientbasicops=r5 ocserverbasicops=r5}
    4b4182228 bluez5=webos26
    1ceebac82 luna-surfacemanager=360 qml-webos-bridge=126
              qml-webos-framework=161-r35 qtwayland-webos=70

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.2',  33, 'https://github.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-3.4',  35, 'https://github.com/webosose/meta-webosose',              '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=44860e4f4', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=a3697874f', ''),
 
 ('meta-raspberrypi',          50, 'https://git.yoctoproject.org/git/meta-raspberrypi',      'branch=dunfell,commit=2e704f5', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
