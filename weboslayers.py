c1c3a7d21cbb79b890263a3ba3de0e5e98994e4c
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2022-01-18 15:34:12 +0900

    webos-image: Update layer pins for 2022-01-18 (build 429)
    meta-webosose as of 2022-01-18 15:38:06 +0900
    
    218e957ec ose: luna-service2=26
    7448fe60d auto-luna-surface-manager=39 luna-surfacemanager=363
              qml-webos-bridge=128 qtbase-plugins-webos=11
    d5bdde417 linux-yocto=webos13
    8d4d34f83 ose: sam=62

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.2',  33, 'https://github.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-3.4',  35, 'https://github.com/webosose/meta-webosose',              '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=a8198bf1d', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=218e957ec', ''),
 
 ('meta-raspberrypi',          50, 'https://git.yoctoproject.org/git/meta-raspberrypi',      'branch=dunfell,commit=934064a', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
