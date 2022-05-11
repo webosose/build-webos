748aa14513b21cf217e9082d36169482b0f9537a
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2022-05-11 15:13:43 +0900

    webos-image: Update layer pins for 2022-05-11 (build 460)
    meta-webosose as of 2022-05-11 15:13:12 +0900
    
    e9c5e09e5 ose: com.webos.app.notification=12
    de7fdb3df luna-sysservice=21

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.2',  33, 'https://github.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-3.4',  35, 'https://github.com/webosose/meta-webosose',              '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=c28d1cc0a', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=e9c5e09e5', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=dunfell,commit=934064a', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
