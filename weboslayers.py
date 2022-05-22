e58b5576939da975e1d31cf9ab22e8a1e6d3ea61
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2022-05-23 07:49:55 +0900

    webos-image: Update to latest revision in Yocto 3.1 Dunfell (build 462)
    meta-webosose as of 2022-05-23 07:40:18 +0900
    
    dfbddba8c webos.inc: Update SANITY_TESTED_DISTROS for Ubuntu 22.04 Jammy Jellyfish
    7c290792a python3-cryptography: backport 3 changes to fix CVE-2020-36242
    a30840554 ls2-helpers=r1 (Fix build with gcc-12)
    21fb98ab1 com.webos.service.unifiedsearch=r2 (Fix build with gcc-12)
    65ab8cf6c bluetooth-sil-bluez5=r6 (Fix build with gcc-12)
    ef0dd91aa com.webos.service.bluetooth2=r6 (Fix build with gcc-12)
    c13414bb7 libgoogleassistant=r8 (fix RDEPENDS for /bin/bash)
    
    oe-core as of 2022-05-10 08:23:12 -1000
    
    0f6ae13d76 scripts/git: Ensure we don't have circular references
    ce6e606ba8 scripts: Make git intercept global
    1567b7cec5 base: Avoid circular references to our own scripts
    e3672b5ccd rootfs-postcommands: fix symlinks where link and output
            path are equal
    00db62342e volatile-binds: Change DefaultDependencies from false to no
    62965ca8ca cve-check: fix symlinks where link and output path are equal
    d8ef964ffe cve-check: add JSON format to summary output
    ce79a724dc cve-update-db-native: let the user to drive the update
            interval
    d0a56ad3a2 cve-update-db-native: update the CVE database once a
            day only
    72e5204bc7 cve-check: no need to depend on the fetch task
    9784b5a062 linux-yocto/5.4: update to v5.4.192
    0b9cbcc4ce busybox: fix CVE-2022-28391
    3f899844b3 libinput: Add fix for CVE-2022-1215
    1c96b8af59 fribidi: Add fix for CVE-2022-25308, CVE-2022-25309 and
            CVE-2022-25310
    7c0345ab10 uninative: Upgrade to 3.6 with gcc 12 support
    d836d47f6a neard: Switch SRC_URI to git repo
    a58a94e451 base: Drop git intercept
    0ff2cff2c1 install/devshell: Introduce git intercept script due to
            fakeroot issues
    af5bcfdc0b cases/buildepoxy.py: fix typo
    e40f202d5f busybox: Use base_bindir instead of hardcoding /bin path
    1ed7bb74d3 python3: ignore CVE-2015-20107
    bb3fc61f0d scripts/contrib/oe-build-perf-report-email.py: remove
            obsolete check for phantomjs and optipng
    ce6d41812a perf-build-test/report: Drop phantomjs and html email
            reports support
    92b6011ab2 cve-check: add json format
    8c0aa16d76 linux-firmware: correct license for ar3k firmware
    9ef2a0d98d boost: don't specify gcc version
    512a8b30c8 tiff: Fix CVE-2022-0891
    
    meta-oe as of 2022-04-19 10:15:37 -0700
    
    8ff12bfffc postgresql: Fix build on riscv
    
    :Issues Addressed:
    [WRO-4101] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=0784db7d', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=8e81d38048', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=0f6ae13d76', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=fdd1dfe6b4', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=8ff12bfffc', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -76,7 +76,7 @@ webos_layers = [
 ('meta-webos-backports-3.2',  33, 'https://github.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-3.4',  35, 'https://github.com/webosose/meta-webosose',              '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=0ca9db0fb', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=dfbddba8c', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=dunfell,commit=934064a', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
