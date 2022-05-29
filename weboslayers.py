6602ed2a511fd116927ad4497f87a08b99398e3e
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2022-05-30 07:10:25 +0900

    webos-image: Update layer pins for 2022-05-30 (build 465)
    meta-webosose as of 2022-05-30 07:00:15 +0900
    
    673019e35 backports-3.4: Upgrade the docker-moby and dependent packages
    fe2be3eb9 webos-fluentbit-plugins=31-r5
    4649cf30c ose: sam=63

--- weboslayers.py
+++ weboslayers.py
@@ -76,7 +76,7 @@ webos_layers = [
 ('meta-webos-backports-3.2',  33, 'https://github.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-3.4',  35, 'https://github.com/webosose/meta-webosose',              '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=30fb0b276', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=673019e35', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=dunfell,commit=934064a', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
