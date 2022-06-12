19488f9189fbe0d295736202e0c322f6314a19c8
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2022-06-13 08:47:12 +0900

    webos-image: Update to latest revision in Yocto 3.1 Dunfell (build 469 part 1/2)
    meta-webosose as of 2022-06-13 08:23:48 +0900
    
    5fdd9c7c2 Revert "python3-cryptography: backport 3 changes to fix CVE-2020-36242"
    ee107c1f5 Revert "remove armnn features on 32bit OSE"
    a89917672 libedgetpu: disable default do_compile from base.bbclass
    9d49cecbd libedgetpu: move one directory up for bitbake to actually find the recipe
    1fd920813 tensorflow-lite=r2 (dlopen libOpenCL.so.1)
    24b7cacc7 python3-pyyaml=v5.4.1
    3433b8f66 edgeai-vision=r2 (inherit pkgconfig)
    93d494ffc stb=r1 (add protocol to SRC_URI)
    1858f7890 armnn=r1 (inherit pkgconfig and ignore array-bounds and deprecated-declarations issues)
    3579736c9 tensorflow-lite=r1 (fix various build issues)
    430cd2f54 arm-compute-library=r1 (fix various build issues)
    e30723dc8 armnn-tensorflow: fix various build issues
    35937ad23 gcc-source=webos3 (use standard Upstream-Status in .patch files)
    0600e9432 gcc-source=webos2 (remove unused 0003-arm-Fix-arm.c-build-error.patch)
    ee7eff4ad tensorflow-lite, armnn, armnn-tensorflow, arm-compute-library: fix various issues
    a5ed4e032 meta-webos-backports-4.0: import as-is in meta-oe/kirkstone
    bbb696a26 meta-webos-backports-4.0: use the new overrides syntax
    1180f40ab webos-preferred-providers.inc: set PREFERRED_PROVIDER_go-native
    b355044fb webos-preferred-providers.inc: remove PREFERRED_PROVIDER_libjson
    945ea3290 webruntime-common.inc, mksnapshot-cross: use shared do_configure
    1e39f1cb9 webruntime: Fix build on host with newer glibc
    8a85633d0 python3=webos2 (remove bash also with ${MLPREFIX})
    35e0e7727 mm-common=webos4 (update based on the version I've submitted to meta-oe)
    e9eb4e2d3 base-passwd: Rename .bbappend to apply on any version
    
    oe-core as of 2022-06-07 11:33:46 +0100
    
    4051d1a3aa openssl: Backport fix for ptest cert expiry
    d009233f36 cve-check: Allow warnings to be disabled
    2bacd7cc67 cve-check: Only include installed packages for rootfs
            manifest
    8a178a728f cve-check: Add helper for symlink handling
    df09cd71b4 cve-check.bbclass: Added do_populate_sdk[recrdeptask].
    cd259a0050 vim: Upgrade 8.2.4912 -> 8.2.5034 to fix 9 CVEs
    096ca5fa8c libxml2: Fix CVE-2022-29824 for libxml2
    2287d591cf ncurses: Fix CVE-2022-29458
    2a97ba89f2 ffmpeg: Fix for CVE-2022-1475
    1cc84e4c51 libsdl2: Add fix for CVE-2021-33657
    df6242b72b ruby: Whitelist CVE-2021-28966 as this affects Windows
            OS only
    4514b1b8ca ruby: Upgrade ruby to 2.7.6 for security fix
    add860e1a6 oeqa/selftest/cve_check: add tests for recipe and image
            reports
    9d5b4fdc7c mobile-broadband-provider-info: upgrade 20220315 -> 20220511
    7f4daf88b7 pcre2: CVE-2022-1586 Out-of-bounds read
    665f981fcc cve-check: Fix report generation
    48ce924dc8 selftest: skip virgl test on alma 8.6
    1ec7c6f0f0 linux-firmware: upgrade 20220411 -> 20220509
    c89bc0fc7f linux-firmware: replace mkdir by install
    de0cafc018 openssl: Minor security upgrade 1.1.1n to 1.1.1o
    970743af34 git: Use CVE_CHECK_WHITELIST instead of CVE_CHECK_IGNORE
    51a92860bd freetype: Fix CVEs for freetype
    7c71434832 tiff: Add patches to fix multiple CVEs
    bbbd258a1c curl: Fix CVEs for curl
    c7d43000ce vim: Upgrade 8.2.4681 -> 8.2.4912
    
    meta-oe as of 2022-05-25 19:34:39 -0700
    
    deee226017 tcpdump: Add fix for CVE-2018-16301
    9f361cff9c opencl-headers: switch to main branch
    a1c7bb2098 fuse: set CVE_PRODUCT to "fuse_project:fuse"
    c9e034fbaa opencl-icd-loader: switch to main branch
    a38c92d8e9 openjpeg: Whitelist CVE-2020-27844 and CVE-2015-1239
    de4b76934c ostree: prevent ostree-native depending on target
            virtual/kernel to provide kernel-module-overlay
    b99a386cd1 python3-cryptography: backport 3 changes to fix
            CVE-2020-36242
    abd7cf838d lua: fix CVE-2022-28805
    a8d82c80a1 atftp: Add fix for CVE-2021-41054 and CVE-2021-46671
    
    :Issues Addressed:
    [WRO-4984]

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=0784db7d', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=0f6ae13d76', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=4051d1a3aa', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=8ff12bfffc', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=deee226017', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-3.4',  35, 'https://github.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-4.0',  36, 'https://github.com/webosose/meta-webosose',              '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=bc755e4e6', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=5fdd9c7c2', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=dunfell,commit=934064a', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
