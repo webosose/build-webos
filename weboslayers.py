8831dfc067b19b04820b9de4454887462a646397
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2022-07-04 08:24:06 +0900

    webos-image: Update layer pins for 2022-07-04 (build 476)
    meta-webosose as of 2022-07-04 08:16:51 +0900
    
    8428a661c ose: com.webos.service.mediaindexer=21

--- weboslayers.py
+++ weboslayers.py
@@ -78,7 +78,7 @@ webos_layers = [
 ('meta-webos-backports-3.4',  35, 'https://github.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-4.0',  36, 'https://github.com/webosose/meta-webosose',              '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=a27007691', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=8428a661c', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=dunfell,commit=934064a', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
