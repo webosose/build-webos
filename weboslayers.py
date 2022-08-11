065fe26530c6b696bd3d4a9f01e5199394b0ac64
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2022-08-11 09:31:56 +0900

    webos-image: Update layer pins for 2022-08-11 (build 490)
    meta-webosose as of 2022-08-11 09:28:12 +0900
    
    73c6491d21 webos_app_generate_security_files.bbclass: Add BPN instead of PN
    33022904fd ose: Add com.webos.app.videocall=1-r0,v0.0.1

--- weboslayers.py
+++ weboslayers.py
@@ -78,7 +78,7 @@ webos_layers = [
 ('meta-webos-backports-3.4',  35, 'https://github.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-4.0',  36, 'https://github.com/webosose/meta-webosose',              '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=e296e599c9', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=73c6491d21', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=dunfell,commit=2081e1b', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
