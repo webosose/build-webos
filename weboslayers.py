092a01129f887e399a8560dc0959c790402a783c
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2022-08-11 09:33:56 +0900

    webos-image: Update to latest revision in Yocto 3.1 Dunfell (build 491 part 1/2)
    meta-webosose as of 2022-08-11 09:28:45 +0900
    
    c1f0392185 gstreamer1.0-plugins-bad=webos4 gstreamer1.0=webos3webosrpi4:
               update qemux86-64 patches
    6ca7bb4576 mpeg2dec=webos1 (Remove executable flag in stack)
    
    oe-core as of 2022-07-28 06:26:48 -1000
    
    73d2b640ad libtirpc: CVE-2021-46828 DoS vulnerability with lots
            of connections
    429c2c89b6 libTiff: CVE-2022-2056 CVE-2022-2057 CVE-2022-2058 DoS
            from Divide By Zero Error
    7c3043df56 qemu: CVE-2022-35414 can perform an uninitialized read on
            the translate_fail path, leading to an io_readx or io_writex crash
    191db3c58b grub2: Fix buffer underflow write in the heap
    152765b74c kernel-fitimage.bbclass: add padding algorithm property
            in config nodes
    80d14a9aaf libjpeg-turbo: Fix CVE-2021-46822
    2bf155d59e gnupg: CVE-2022-34903 possible signature forgery via
            injection into the status line
    3f40d5f095 build-appliance-image: Update to dunfell head revision
    597eef3b2f linux-yocto-rt/5.4: fixup -rt build breakage
    c954fc1097 linux-yocto/5.4: update to v5.4.205
    aad2a33008 curl: Fix CVE-2022-32206, CVE-2022-32207, and CVE-2022-32208
    5cb48712e0 cve-extra-exclusions.inc: Use CVE_CHECK_WHITELIST
    
    meta-oe as of 2022-08-02 06:59:38 -0700
    
    f22bf6efaa meta-oe: Add leading whitespace for append operator
    a04c5444c9 bigbuckbunny-1080p: update SRC_URI
    3ba409127c ntfs-3g-ntfsprogs: upgrade to 2022.5.17
    
    :Issues Addressed:
    [WRO-9071] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.46,commit=7fc4cffe', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=bba069463c', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=dunfell,commit=73d2b640ad', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=52cee67833', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=dunfell,commit=f22bf6efaa', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -78,7 +78,7 @@ webos_layers = [
 ('meta-webos-backports-3.4',  35, 'https://github.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-4.0',  36, 'https://github.com/webosose/meta-webosose',              '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=73c6491d21', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=c1f0392185', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=dunfell,commit=2081e1b', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
