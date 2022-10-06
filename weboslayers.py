ca732660956e544fd48cb1a4c0778a806a6a223c
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2022-10-06 10:38:23 +0900

    webos-image: Update to latest revision in Yocto 3.3 Hardknott (build 510)
    meta-webosose as of 2022-10-06 10:23:55 +0900
    
    22ecd26e {hardknott-stop} webos: layer.conf: Update to hardknott release name series
    00a6c9db tensorflow-lite: use system abseil-cpp and don't install libabsl*.so libraries
    b8f1e219 abseil=v20210324.2+git
    1b30105e gcc-source: update patches to apply on 10.3 version
    9b329b06 flatbuffers: remove unnecessary bbappend
    2509f731 systemd-serialgetty: fix LIC_FILES_CHKSUM as it is in oe-core hardknott branch
    ea66a25a webos.inc: update DEFAULT_WARN_QA to include native-last
    a7c55870 go-dep: remove unnecessary bbappend
    4e00908b rpi: gstreamer1.0-omx: update 0003-decrease-ranking-of-omxh264dec.patch to apply cleanly
    1367af84 busybox: update the patches to apply on 1.33.0 version from oe-core
    ce5e7d59 bluez5: update the patches to apply on 5.56 version from oe-core
    7522fdc9 containerd-docker: remove bbappend
    514ca955 rpi: u-boot: drop bbappend
    83ba7429 webos.inc: webkitgtk, mpich: don't use -Werror=return-type
    daedb563 opkg: update patches to apply on 0.4.4 version
    79227a8a pulseaudio: import older pulseaudio.inc which is still using autotools from oe-core
    697c51ae webos: PNBLACKLIST: update for Yocto 3.3 Hardknott
    e77ce14e dbus: use PN in RDEPENDS
    70f0e46b strace=webos1 gawk=webos1 (remove locale-base-en-us.iso-8859-1 runtime dependency)
    7588d1fd bats: update file-rdeps QA issue fix about /bin/bash
    fd0321fb ntopng, linuxconsole, influxdb, libpam, spirv-tools, rpm, android-tools: fix build-deps QA issue with bash
    d948e9f0 rpi: u-boot: update patches to apply on 2020.10 version
    ef1ba74a qemux86-64: gstreamer1.0-plugins-bad: update patches to work with 1.18.6 version
    0e8440b1 webos: gstreamer1.0-plugins-bad: update qemux86-64 patches to apply on 1.18.3 version
    90f906dc rpi: gstreamer1.0-omx: update patches to apply on 1.18.1 version
    8a8b6d2c rpi: gstreamer1.0-plugins-good: update patches to apply on 1.18.3 version
    9608a50e gstreamer*: rename bbappends to match new 1.18.0 version from oe-core
    b0c42526 mesa=webosrpi6 (update patch to apply on 20.3.1 version)
    6dd2b50a {hardknott-start} meta-webos-backports-3.3: delete
    
    oe-core as of 2022-04-05 16:51:49 +0100
    
    2289 commits from:
    https://github.com/openembedded/openembedded-core/compare/9cce475ffc...fbbb689c8d
    
    bitbake as of 2022-04-16 23:03:09 +0100
    
    159 commits from:
    https://github.com/openembedded/bitbake/compare/38bff0ba...d01d8326
    
    meta-oe as of 2022-04-29 18:43:20 -0700
    
    1559 commits from:
    https://github.com/openembedded/meta-openembedded/compare/f3f7a5f1a4...786d3aa313
    
    meta-raspberrypi as of 2021-11-26 17:25:09 +0000
    
    106 commits from:
    https://github.com/agherzan/meta-raspberrypi/compare/e5e976d...b4ec97e
    
    meta-security as of 2021-12-03 14:15:13 -0800
    
    94 commits from:
    https://git.yoctoproject.org/meta-security/log/?h=c40e1e8
    
    meta-updater as of 2022-03-29 16:42:26 +0200
    
    0 commits from:
    https://github.com/uptane/meta-updater/compare/094288c...bbbc640
    
    meta-virtualization as of 2022-04-26 17:04:55 -0400
    
    193 commits from:
    https://git.yoctoproject.org/meta-virtualization/log/?h=e4ea75c
    
    :Issues Addressed:
    [WRO-12997] CCC: Upgrade to Yocto 3.3 Hardknott
    [PLAT-124341] Create GPVB with Yocto 3.3 Hardknott

--- weboslayers.py
+++ weboslayers.py
@@ -58,32 +58,31 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.48,commit=38bff0ba', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=1.50,commit=d01d8326', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=gatesgarth,commit=9cce475ffc', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=hardknott,commit=fbbb689c8d', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=gatesgarth,commit=f3f7a5f1a4', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=hardknott,commit=786d3aa313', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=gatesgarth,commit=094288c', ''),
-('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=gatesgarth,commit=2e5df1c', ''),
+('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=hardknott,commit=bbbc640', ''),
+('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=hardknott,commit=e4ea75c', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.3.1,commit=39eeba2', ''),
 
-('meta-webos-backports-3.3',  34, 'https://github.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-3.4',  35, 'https://github.com/webosose/meta-webosose',              '', ''),
 ('meta-webos-backports-4.0',  36, 'https://github.com/webosose/meta-webosose',              '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=96334ea5', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=22ecd26e', ''),
 
-('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=gatesgarth,commit=e5e976d', ''),
+('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=hardknott,commit=b4ec97e', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-updater',        52, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
 ('meta-webos-smack',          75, 'https://github.com/webosose/meta-webosose.git',          '', ''),
-('meta-security',             77, 'https://git.yoctoproject.org/git/meta-security',         'branch=gatesgarth,commit=3daf99f', ''),
+('meta-security',             77, 'https://git.yoctoproject.org/git/meta-security',         'branch=hardknott,commit=c40e1e8', ''),
 ]
