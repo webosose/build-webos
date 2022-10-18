086f1cc061200ea24e25bba2132e0d290aebc7f0
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2022-10-18 10:05:43 +0900

    webos-image: Update to latest revision in Yocto 4.0 Kirkstone (build 516 part 1/5)
    meta-webosose as of 2022-10-18 10:02:10 +0900
    
    2df1674e meta-webos-backports-4.2: add backports layer for 4.2 Mickledore
    a0966566 re2: move bbappend to meta-webos-backports-4.1
    fbee80f1 meta-webos-backports-4.1: add backports layer for 4.1 Langdale
    c6c27384 webos.inc: set QEMU_EXTRAOPTIONS for supported MACHINEs
    a2cc8e08 nodejs-module-usocket=r6 (use webos_npm_env)
    767087c1 webos_npm_env.bbclass: add more variables to pass --nodedir in node-gyp.
    8887f6e3 image_types_wic.bbclass: import from oe-core with a fix for multilib
    
    oe-core as of 2022-09-30 09:35:23 -1000
    
    e728d0965d coreutils: add openssl PACKAGECONFIG
    46b703dbbd glibc-locale: explicitly remove empty dirs in ${libdir}
    b1883d725e lttng-tools: Disable on riscv32
    a3d6742ac0 stress-cpu: disable float128 math on powerpc64 to avoid
            SIGILL
    5aac691040 create-pull-request: don't switch the git remote protocol
            to git://
    035fddfa35 lttng-tools: Disable on qemuriscv32
    cc3d1034d5 tzdata: update to 2022d
    01710ddade bind: upgrade 9.18.6 -> 9.18.7
    daae4cada7 bind: upgrade 9.18.5 -> 9.18.6
    b2be1f5e7e rpm: Remove -Wimplicit-function-declaration warnings
    49a8753221 rpm: update 4.17.0 -> 4.17.1
    9431be33d8 rsync: update 3.2.4 -> 3.2.5
    4a4097ed1d rsync: update 3.2.3 -> 3.2.4
    d95303abef webkitgtk: Update to 2.36.7
    342a05cc65 webkitgtk: Upgrade to 2.36.6 minor update
    9837a62cc4 vim: Upgrade 9.0.0541 -> 9.0.0598
    e384b754eb binutils : Fix CVE-2022-38127
    
    bitbake as of 2022-09-28 06:47:55 -1000
    
    6603c3e3 asyncrpc/client: Fix unix domain socket chdir race issues
    987712c4 gitsm: Error out if submodule refers to parent repo
    bb5c4322 Fix npm to use https rather than http
    e591325b bitbake: Add copyright headers where missing
    63bb5591 siggen: Fix insufficent entropy in sigtask file names
    f386298f runqueue: Drop deadlock breaking force fail
    d1b84e3c runqueue: Improve deadlock warning messages
    513bfd77 runqueue: Ensure deferred tasks are sorted by multiconfig
    
    meta-oe as of 2022-09-25 11:03:26 -0400
    
    bd2d8fba76 minicoredumper: retry elf parsing as long as needed
    de2bbc5ef4 libcec: fix runtime dependencies for ${PN}-examples
    fc9c8a3332 frr: Security fix CVE-2022-37035
    c5b5f631fc lmdb: Don't inherit base
    7eff264695 audit: Revert the tweak done in configure step in do_install
    088eaf9ea9 postgreql: Fix pg_config not working after buildpaths patch
    73e66e5ea3 postgresql: upgrade 14.4 -> 14.5
    8c62aaa67e php: upgrade 8.1.9 -> 8.1.10
    
    meta-updater as of 2022-10-12 17:33:54 +0200
    
    7935897 Merge pull request #55 from quaresmajose/bbfatal_log
    3e7c55c image_types_ostree: use bbfatal_log when do_image_garagesign
            fails
    ee7322c Merge pull request #53 from quaresmajose/errcode
    dd129dc image_types_ostree: show the errorcode on the push failed
            warning
    0207f86 Merge pull request #50 from mdfr74/kirkstone
    a2d9042 Fix typos created by 6159909
    
    :Issues Addressed:
    [WRO-13280] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -58,22 +58,22 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=ac576d6f', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=6603c3e3', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=d19cd09b43', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=e728d0965d', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=05dcac9847', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=bd2d8fba76', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=kirkstone,commit=03fc4eb', ''),
+('meta-updater',              15, 'https://github.com/uptane/meta-updater.git',             'branch=kirkstone,commit=7935897', ''),
 ('meta-virtualization',       16, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=9482648', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.3.1,commit=39eeba2', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=980d8e13', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=2df1674e', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=dacad93', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
