c8f4eba8f1263c11c678b10255f2b315d9658665
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2022-11-04 08:27:59 +0900

    webos-image: Update layer pins for 2022-11-04 (build 521)
    meta-webosose as of 2022-11-04 08:21:31 +0900
    
    eeb3a3aac ose: com.webos.service.camera=29
    6e2374f63 qtwayland=webos34
    34598c521 com.webos.app.videocall=6  com.webos.app.home=28
    d3b07aa9e ose: webruntime_94=r58.2 wam=r50
    751314c1f audiod=54-r32
    fccf12ac5 com.webos.app.notification=14
    ccf871394 com.webos.app.settings=23
    03501740e Add webos_ls2_api_list.bbclass, webos_image.bbclass: LS2 API Info extraction
    65e9b16c3 externalsrc.bbclass: import from kirkstone with additional fix for big git repos
    aa8d47602 solana-program-library: add openssl dependency and export OPENSSL_NO_VENDOR
    30ed2fbfb rust: backport 1.63.0 version from 4.1 Langdale
    c0799da06 Revert "ose: Add webos_{cargo,rust-common}.bbclass, {cargo,rust}-bin-cross"
    f45bc985f solana*,sugar: Fix various issues
    af4ebdf05 webos_cargo.bbclass: Enable network access in webos_cargo_do_compile task
    f981ed27a nodejs-module-usocket=r8 (use npm:// fetcher and npm.bbclass)

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-4.1',  30, 'https://github.com/webosose/meta-webosose',             '', ''),
 ('meta-webos-backports-4.2',  31, 'https://github.com/webosose/meta-webosose',             '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=eccd13fe4', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=eeb3a3aac', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=dacad93', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
