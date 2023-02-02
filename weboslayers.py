9ef784564e5e41b6f1e460e5ae5f46c197e32483
Hyunjae Shin <hyunjae92.shin@lgepartner.com>
2023-02-02 10:13:27 +0900

    webos-image: Update to latest revision in Yocto 4.0 Kirkstone (build 559 part 1/2)
    bitbake as of 2023-01-30 08:37:10 +0000
    
    ed0dcc40 bb/utils: include SSL certificate paths in export_proxies
    
    :Issues Addressed:
    [WRP-7956] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -58,7 +58,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=7e268c10', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=ed0dcc40', ''),
 
 ('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=a8c8290238', 'oe-core'),
 
