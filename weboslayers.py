c90f84e5edd3b634531b6113ffdf49c9783a0f20
jaekyu.lee <jaekyu.lee@lge.com>
2023-03-21 23:32:54 +0900

    webos-image: Update to latest revision in Yocto 4.0 Kirkstone (build 579)
    oe-core as of 2023-03-14 05:40:43 -1000
    
    8b41b5d2e4 vala: Fix install conflict when enable multilib.
    b2892b3b6f lua: Fix install conflict when enable multilib.
    e09c50e01d linux: inherit pkgconfig in kernel.bbclass
    3fbcaaa013 oeqa/sdk: Improve Meson test
    1f30dedee8 meson: Fix wrapper handling of implicit setup command
    01eeabdd6f vim: add missing pkgconfig inherit
    3fd47697df systemd: add group sgx to udev package
    9afb0e2f78 oeqa/selftest/prservice: Improve debug output for failure
    0ae1ed426e python3-setuptools-rust-native: Add direct dependency of
            native python3 modules
    47528fa2aa binutils: Fix nativesdk ld.so search
    e2c6593b1e iso-codes: upgrade 4.12.0 -> 4.13.0
    7819b6b18d linux-yocto-rt/5.15: update to -rt59
    5f969d8a3b linux-yocto/5.15: update to v5.15.96
    a8a3d9d246 linux-yocto/5.15: update to v5.15.94
    b307a79caf sudo: update 1.9.12p2 -> 1.9.13p3
    017346be6c libmicrohttpd: upgrade 0.9.75 -> 0.9.76
    d0b1f61eb1 shadow: ignore CVE-2016-15024
    f2782b4cac tiff: fix multiple CVEs
    58e212a610 harfbuzz: Security fix for CVE-2023-25193
    05b1222815 gnutls: fix CVE-2023-0361 timing side-channel in the TLS
            RSA key exchange code
    eadf28a064 glibc: Security fix for CVE-2023-0687
    d5390008c3 epiphany: Security fix for CVE-2023-26081
    05b1222815 gnutls: fix CVE-2023-0361 timing side-channel in the TLS
            RSA key exchange code
    eadf28a064 glibc: Security fix for CVE-2023-0687
    d5390008c3 epiphany: Security fix for CVE-2023-26081
    
    bitbake as of 2023-03-14 15:05:00 +0000
    
    a29ffcc1 cookerdata: Drop dubious exception handling code
    e66012bf cookerdata: Improve early exception handling
    b9ae7164 cookerdata: Remove incorrect SystemExit usage
    e7df13a6 utils: Allow to_boolean to support int values
    
    meta-oe as of 2023-03-13 08:58:37 -0400
    
    9cf4ebeb3d nodejs: Upgrade 16.19.0 -> 16.19.1
    acd692b775 nlohmann-json: Allow empty main package for SDK
    9d4bbb9b32 php: Upgrade to 8.1.16
    64fb11e1a6 redis: upgrade 6.2.8 -> 6.2.11
    64fbb27b5a redis: upgrade 7.0.7 -> 7.0.9
    6623bf1ca8 postgresql: fix CVE-2022-41862
    75cc182f38 cifs-utils: fix CVE-2022-27239 CVE-2022-29869
    54960c549b grpc: upgrade 1.46.6 -> 1.46.7
    4676fa71c6 poppler: fix CVE-2021-30860
    109b546307 liblockfile: fix do_install failure when ldconfig is
            not installed
    0342b7b6c3 fluidsynth: update SRC_URI to remove non-existing 2.2.x
            branch
    ace7fca1d9 mpd: Upgrade to 0.23.12 release
    34da9c55df ncmpc: Upgrade to 0.47
    6b148cd433 mpd: Upgrade to 0.23.9
    b6dfef7d8b mpd: Update to 0.23.8
    d413a66d20 python3-pillow: add tk to RDEPENDS ptest pkg only if x11
            in DISTRO_FEATURES
    fb99d19bac multipath-tools: fix CVE-2022-41974
    dfcbe97b55 mdns: use git fetcher
    31801ca33e exiv2: fix SRC_URI
    32adef4566 nss: fix cross-compilation error
    
    meta-virtualization as of 2023-03-17 15:32:16 -0400
    
    bfa6727 lxc: add -L and -f for curl in
            templates-use-curl-instead-of-wget.patch
    a9361e9 lxc: backport changes from master for
            templates-use-curl-instead-of-wget.patch
    
    meta-clang as of 2023-03-10 08:59:00 -0800
    
    7e3f2a4 android-tools: allow removing fastboot from TOOLS_TO_BUILD
    547fbf8 android-tools: make it work with devtool
    
    :Issues Addressed:
    [WRP-10791] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -58,18 +58,18 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=9bbdedc0', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=a29ffcc1', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=9e8cbf46fe', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=8b41b5d2e4', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=82c75b466e', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=9cf4ebeb3d', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=kirkstone,commit=15ea5a1', ''),
-('meta-virtualization',       17, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=a517e15', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=kirkstone,commit=7e3f2a4', ''),
+('meta-virtualization',       17, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=bfa6727', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.4.1,commit=v6.4.1', ''),
 
