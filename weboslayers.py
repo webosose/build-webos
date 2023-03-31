468b4e28660ea0c9eaac1277dc15165dba2e0e11
jaekyu.lee <jaekyu.lee@lge.com>
2023-03-31 10:20:07 +0900

    webos-image: Update to latest revision in Yocto 4.0 Kirkstone (build 582 part 1/2)
    oe-core as of 2023-03-23 06:42:22 -1000
    
    08ab1f02da systemd: fix wrong nobody-group assignment
    58dfc69f52 cracklib: update github branch to 'main'
    2429ae8340 pybootchartui: Fix python syntax issue
    ac9550404d tzcode-native: fix build with gcc-13 on host
    cf96296296 tzdata: use separate B instead of WORKDIR for zic output
    8e047570ca timezone: use 'tz' subdir instead of ${WORKDIR} directly
    213ed03f5a lib/resulttool: fix typo breaking resulttool log --ptest
    b89abb0f4c oeqa rtc.py: skip if read-only-rootfs
    0fce158e59 image_types: fix multiubi var init
    7582421fc3 systemd.bbclass: Add /usr/lib/systemd to searchpaths as well
    4ad488d245 devtool/upgrade: do not delete the workspace/recipes
            directory
    8a8a86076f git: ignore CVE-2023-22743
    2e502df061 systemd: fix CVE-2022-4415
    c80ede99b6 curl: Add fix for CVE-2023-23914, CVE-2023-23915
    
    :Issues Addressed:
    [WRP-11775] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=a29ffcc1', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=7df46e003e', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=08ab1f02da', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=9cf4ebeb3d', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
