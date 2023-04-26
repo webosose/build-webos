31e029ffe55d44077e740765d8348e65a3d9aa12
jaekyu.lee <jaekyu.lee@lge.com>
2023-04-27 01:32:16 +0900

    webos-image: Update layer pins for 2023-04-27 (build 582 part 2/2)
    meta-webosose as of 2023-04-26 03:09:39 +0900
    
    6308ef18 luna-surfacemanager=r58-386 qml-webos-components=r21-58
            qtbase-plugins-webos=r7-15 qtwayland-webos=r20-85
    6319f3fe ose: sleepd=16
    80d89599 ose: cambufferlib=6
    5e951cd6 gstreamer1.0-plugins-webos=5-r3
    67cdb460 g-media-pipeline=gav.42
    64af2c7c audiod=61-r35
    d3c26e69 bmap-tools: backport fix for renamed branch
    cdd367b7 linux-raspberrypi=webosrpi34: update seed-voicecard for
            6.1 kernel
    670868db linux-raspberrypi: Add Upstream-Status and more info into
            seed-voicecard patch

--- weboslayers.py
+++ weboslayers.py
@@ -76,7 +76,7 @@ webos_layers = [
 ('meta-webos-backports-4.1',  30, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-backports-4.2',  31, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=27c1babd', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=6308ef18', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=dacad93', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
