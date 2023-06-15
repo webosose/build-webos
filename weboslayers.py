68838abf48d64376e9a280e2e28041a8906c9033
Martin Jansa <martin2.jansa@lgepartner.com>
2023-06-15 09:23:26 +0200

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2023-06-09 (build 611 part 1/2)
    oe-core as of 2023-06-09 06:04:24 -1000
    
    18a9596ae7 selftest/reproducible: Allow native/cross reuse in test
    56fdf57d99 systemd-networkd: backport fix for rm unmanaged wifi
    10b9fdce35 openssh: Remove BSD-4-clause contents completely from
            codebase
    e806c625d9 python3-requests: fix for CVE-2023-32681
    5140fdf341 openssl: Upgrade 3.0.8 -> 3.0.9
    b093db144b perl: fix CVE-2023-31484
    4691bc257d curl: Correction for CVE-2023-27536
    0a09194f3d go: fix CVE-2023-24539 html/template improper sanitization
            of CSS values
    d4ee3ad883 sysstat: Fix CVE-2023-33204

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=0c6f86b6', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=717b9f18a5', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=18a9596ae7', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=f95484417e', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
