e9d26b2d8d525f779bcfea931213e6e1c86adafb
Martin Jansa <martin2.jansa@lgepartner.com>
2023-06-15 09:24:59 +0200

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2023-06-23 (build 615)
    meta-webosose as of 2023-09-07 13:48:38 +0900
    
    9668822c uninative: call patchelf-uninative only when needed
    15ab8bcd systemd: add missing Upstream-Status
    5fa51571 googletest-clang: add FILESEXTRAPATH for .patch file
            from meta-oe
    
    oe-core as of 2023-06-23 04:17:20 -1000
    
    7949e786cf build-appliance-image: Update to kirkstone head revision
    59f1a1b232 lib/terminal.py: Add urxvt terminal
    dad053c4b9 staging.bbclass: do not add extend_recipe_sysroot to
            prefuncs of prepare_recipe_sysroot
    9ce30faa48 oeqa/selftest/cases/devtool.py: skip all tests require
            folder a git repo
    5dec509c33 kmscube: Correct DEPENDS to avoid overwrite
    44b75c7442 kernel: don't force PAHOLE=false
    c89090e148 conf: add nice level to the hash config ignred variables
    f9b1a56c50 go.bbclass: don't use test to check output from ls
    86e8e31a41 iso-codes: upgrade 4.13.0 -> 4.15.0
    6b82d99818 nasm: fix CVE-2022-46457
    0e17a5a4f0 Revert "ipk: Decode byte data to string in manifest
            handling"
    3a5ce6f809 e2fsprogs: fix ptest bug for second running
    11d3f1c825 systemd-systemctl: support instance expansion in WantedBy
    035b5d918e vim: upgrade 9.0.1429 -> 9.0.1527
    2057c5a6e7 linux-yocto/5.15: update to v5.15.113
    6180d99b8a linux-yocto/5.15: update to v5.15.112
    ea29961df7 linux-yocto/5.15: update to v5.15.111
    0c328f0b04 linux-yocto/5.15: update to v5.15.110
    fe55be7f4a linux-yocto/5.15: update to v5.15.109
    afce764918 glibc: stable 2.35 branch updates
    c7e1611ab0 cve-update-nvd2-native: added the missing http import
    b8ae26aabe cve-update-nvd2-native: new CVE database fetcher
    da1e7adf33 webkitgtk: fix CVE-2022-46700
    d4a872b6ac webkitgtk: fix CVE-2022-42867
    8f71f60af3 webkitgtk: fix CVE-2022-46699
    1560326103 webkitgtk: fix CVE-2022-46691
    cf741646f4 cups: Fix CVE-2023-32324
    2da7a711ca openssh: fix CVE-2023-28531
    6e0d694ea1 uninative: Upgrade to 4.0 to include latest gcc 13.1.1
    dbf2a81d42 uninative: Upgrade to 3.10 to support gcc 13
    
    meta-oe as of 2023-06-19 09:17:01 -0400
    
    a82d92c8a6 ExprTk: Update package to release/0.0.2
    8fb1e24fcc Googletest: Adapt googletest 1.11.0 with gcc11
    5790310da3 samba: fix CVE-2021-44758
    0393024cc5 samba: fix CVE-2022-41916
    84544d6440 samba: fix CVE-2022-45142
    9d203efe8f samba: fix CVE-2022-3437
    25dcf4d65b wireshark: CVE-2023-2952 XRA dissector infinite loop
    420acd8735 python3-sqlparse: fix for CVE-2023-30608
    9ea78f00a4 python3-django: upgrade 3.2.12 -> 3.2.19
    e43d068788 python3-django: fix for CVE-2023-31047
    26b9ab59fc redis: use the files path correctly
    61c1fd0693 meta-oe: add pahole to NON_MULTILIB_RECIPES
    7a87a24786 openldap: Fix CVE-2023-2953
    5adcef0348 c-ares: ignore CVE-2023-31124
    7ee1d68fe1 opencv: Fix for CVE-2023-2617
    5bb0b48228 lapack: add packageconfig for lapacke
    bdad2a789e python3-werkzeug: fix for CVE-2023-23934
    fca236e75a gnulib: Update recipe name to 2018-12-18
    c34b8aad57 c-ares: fix CVEs CVE-2023-32067 and CVE-2023-31130
    c7f4255d0e wireshark: Fix CVE-2023-2858 & CVE-2023-2879
    c7a5562f77 wireshark: CVE-2023-2856 VMS TCPIPtrace file parser crash
    a506fa6eac wireshark: CVE-2023-2855 Candump log file parser crash
    
    meta-clang as of 2023-06-22 07:53:13 -0700
    
    3523149 clang: fix build with yocto uninative gcc 13

--- weboslayers.py
+++ weboslayers.py
@@ -60,15 +60,15 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=0c6f86b6', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=18a9596ae7', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=7949e786cf', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=f95484417e', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=a82d92c8a6', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=kirkstone,commit=71321dd', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=kirkstone,commit=3523149', ''),
 ('meta-virtualization',       17, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=b3b3dbc', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.5.1,commit=v6.5.1', ''),
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-4.2',  31, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-backports-4.3',  32, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=75f4fc61', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=9668822c', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=43683cb', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
