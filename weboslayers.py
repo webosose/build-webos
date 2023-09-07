5724c3eecf13315a20d8821102f6030dd6f4441d
ywbyun0815 <yw.byun@lge.com>
2023-09-07 13:27:11 +0900

    webos-image: Update layer pins for 2023-09-07 (build 611 part 2/2)
    meta-webosose as of 2023-09-06 13:45:24 +0900
    
    60998aec ose: sam=67
    9345ed83 c-ares: backport for fix DNS Resolution error
    38d3cbf0 ose: always use BPN in FILESEXTRAPATHS
    605c2406 clang_cmake.bbclass: use append operator for DEPENDS
    a2b7bbc6 meta-webos-backports-4.3: add backports layer for 4.3 Nanbield
    ccce1801 webos-recipe-blacklist.inc: remove
            packagegroup-qt6-commercial-modules
    c1fc7650 qttools: add PACKAGECONFIG for clang dependency

--- weboslayers.py
+++ weboslayers.py
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-4.2',  31, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-backports-4.3',  32, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=53284c5f', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=60998aec', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=43683cb', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
