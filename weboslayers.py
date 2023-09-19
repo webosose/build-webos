b6806907103205534657bc8346b2eda758825ad2
Martin Jansa <martin2.jansa@lgepartner.com>
2023-09-19 10:31:05 +0200

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2023-09-13 (build 649)
    meta-webosose as of 2023-09-29 15:44:12 +0530
    
    06c9f60b webos: webos_oss_pkg_info.bbclass: avoid unnecessary
             oe.path.join
    84e80a5d webos: iotivity=r9 (fix build with inheritted ccache)
    b4b91383 webos-backports-4.1: smack-test: backport changes to use
             python3
    2dbf960f webos-backports-4.2: mongodb, opengl-es-cts, libcyusbserial,
             tcpreplay: move backports from 4.3 to 4.2
    45d3e8ba webos: com.webos.service.camera=r12 (fix build with gcc-13)
    
    oe-core as of 2023-09-24 10:53:54 -1000
    
    d90e4d5e3c build-appliance-image: Update to kirkstone head revision
    87eee047cf Revert "oeqa/utils/gitarchive: fix tag computation when
            creating archive"
    6c7fef37d4 build-appliance-image: Update to kirkstone head revision
    a3f566fcbf cargo.bbclass: set up cargo environment in common do_compile
    55529a5cb4 dbus: Specify runstatedir configure option
    79498ea0e9 tcl: prevent installing another copy of tzdata
    af9f392a5e wic: fix wrong attempt to create file system in upartitioned
            regions
    29fe45fe88 build-sysroots: Add SUMMARY field
    b95c6a5278 resulttool/report: Avoid divide by zero
    d0f8d5915a oeqa/utils/gitarchive: fix tag computation when creating
            archive
    c88343380b scripts/create-pull-request: update URLs to git repositories
    f6bb8438a1 externalsrc: fix dependency chain issues
    909fd25c2e pseudo: Fix to work with glibc 2.38
    5f78a010a4 vim: Upgrade 9.0.1664 -> 9.0.1894
    d5ba354605 vim: upgrade 9.0.1592 -> 9.0.1664
    72af322b6b vim: update obsolete comment
    4910b1e46a tar: upgrade 1.34 -> 1.35
    e8becc003d binutils: stable 2.38 branch updates
    b834674ada glibc: stable 2.35 branch updates
    e6592fc830 gcc: Fix -fstack-protector issue on aarch64
    860ecdbbf5 binutils: Fix CVE-2022-48065
    b04f40d7af cups: fix CVE-2023-32360
    afdc322ecf go: Fix CVE-2023-39319
    26309ba6ef yocto-uninative: Update to 4.3
    1a19a101ce gdb: Fix CVE-2023-39128
    8f956bc199 webkitgtk: fix CVE-2022-48503
    29c6287287 flac: fix CVE-2020-22219
    9a2ad95caf libxml2: Fix CVE-2023-39615
    ebbdbb68a7 qemu: fix CVE-2021-3638
    38709b0d35 dropbear: fix CVE-2023-36328
    ebb224e65a python3-pygments: Fix CVE-2022-40896
    
    :Issues Addressed:
    [WRP-27901] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=41b668448', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=47a1dd7f38', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=d90e4d5e3c', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=a88cb922f9', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -77,7 +77,7 @@ webos_layers = [
 ('meta-webos-backports-4.2',  31, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-backports-4.3',  32, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=70b07ed4', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=06c9f60b', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=80a12f7', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
