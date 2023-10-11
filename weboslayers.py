2791d95a7bd97d76ef35e614060cccc315097c07
Martin Jansa <martin2.jansa@lgepartner.com>
2023-10-11 21:16:12 +0200

    weboslayers.py: add meta-webos-backports-5.0 layer (build 664 part 4/5)
    meta-webosose as of 2023-11-08 13:25:35 +0530
    
    360ac002 meta-webos-backports-5.0: add backports layer for 5.0
             Scarthgap
    
    :Issues Addressed:
    [WRQ-1890] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -76,8 +76,9 @@ webos_layers = [
 ('meta-webos-backports-4.1',  30, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-backports-4.2',  31, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-backports-4.3',  32, 'https://github.com/webosose/meta-webosose.git',          '', ''),
+('meta-webos-backports-5.0',  33, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=f295f68d', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=360ac002', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=59a6a1b', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
