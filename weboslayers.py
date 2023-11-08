64361bfb5e232fc5c67a41478c6ea019fd8a4c05
Martin Jansa <martin2.jansa@lgepartner.com>
2023-11-08 16:16:28 +0100

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2023-11-08 (build 669 part 1/2)
    oe-core as of 2023-11-08 04:10:02 -1000
    
    4bb6373e5f python3-jinja2: Fixed ptest result output as per the
            standard
    b4e5e02ab5 cve-check: don't warn if a patch is remote
    4b449d5dcb cve-check: slightly more verbose warning when adding the
            same package twice
    1245649fd2 cve-check: sort the package list in the JSON report
    afc30fc07d xserver-xorg: Fix for CVE-2023-5367 and CVE-2023-5380
    dbef9bf56f libwebp: Fix CVE-2023-4863
    3fd5701a86 binutils: Fix CVE-2022-47010
    
    bitbake as of 2023-11-09 12:49:31 +0000
    
    a7f6c3e67 Fix disk space monitoring on cephfs
    3505d8d8c runqueue: convert deferral messages from bb.note to bb.debug
    e452c6d7b tinfoil: Do not fail when logging is disabled and full
            config is used
    161ab0d5b bitbake-getvar: Make --quiet work with --recipe
    
    meta-oe as of 2023-11-03 10:52:59 -0400
    
    7da6cb848b indent: fix CVE-2023-40305
    b4bee1f709 packagegroup-meta-multimedia: restore x11 restriction
            for projucer
    579558c87f mbedtls: upgrade 3.4.0 -> 3.5.0
    8274d201cb suiteparse: Adapt to upstream branch name changes
    efe1115b0f python-blivet: Adapt to upstream branch name changes
    54ebe876ba grubby: Update branchname to match upstream
    cc322bb5fa ntfs-3g-ntfsprogs: Upgrade 2022.5.17 to 2022.10.3
    
    meta-clang as of 2023-11-09 22:40:05 -0800
    
    79169d9 Fix packaging error for libclang
    
    meta-virtualization as of 2023-11-21 04:07:47 +0000
    
    67b0ef42 kubernetes: Fix CVE-2023-2727, CVE-2023-2728
    86126c9b kubernetes: Fix CVE-2023-2431
    72ef3ba3 kubernetes: Adjust patches to resolve error that occur
            with devtool
    478a9180 packagegroup-container: require ipv6 for podman
    
    :Issues Addressed:
    [WRQ-2333] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -58,18 +58,18 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=6c1ffa909', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=a7f6c3e67', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=0eb8e67aa6', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=4bb6373e5f', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=79a6f60dab', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=7da6cb848b', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
-('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=kirkstone,commit=eb8b3a5', ''),
-('meta-virtualization',       17, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=2d8b3cba', ''),
+('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=kirkstone,commit=79169d9', ''),
+('meta-virtualization',       17, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=67b0ef42', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.6.0,commit=v6.6.0', ''),
 
