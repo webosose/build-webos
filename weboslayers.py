30de66afa892f4ec7c2adefc8c8f5b1261b3dbda
ywbyun0815 <yw.byun@lge.com>
2023-11-27 10:04:09 +0900

    webos-image: Update layer pins for 2023-11-27 (build 666)
    meta-webosose as of 2023-11-27 09:07:55 +0900
    
    f89f6c6a webos: com.webos.app.camera=17

--- weboslayers.py
+++ weboslayers.py
@@ -78,7 +78,7 @@ webos_layers = [
 ('meta-webos-backports-4.3',  32, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-backports-5.0',  33, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=bfd6ed82', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=f89f6c6a', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=59a6a1b', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
