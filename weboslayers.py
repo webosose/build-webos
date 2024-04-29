a7263b2e8ffe499a6a297dc89ae0c496789051fe
Martin Jansa <martin2.jansa@lgepartner.com>
2024-04-29 13:24:06 +0200

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2024-04-28 (build 729 part 1/2)
    oe-core as of 2024-04-26 06:15:55 -0700
    
    8a50230120 rootfs-postcommands.bbclass: Only set DROPBEAR_RSAKEY_DIR
            once
    016387557c glibc: Update to latest on stable 2.35 branch
    9ad10bf355 go: Fix for CVE-2023-45288
    e63819fbab gnutls: fix CVE-2024-28835
    18c4f65934 gnutls: fix CVE-2024-28834
    
    meta-oe as of 2024-04-28 13:10:23 -0400
    
    5a6f7925bd python3-twisted: Add python3-typing-extensions to RDEPENDS
    8d40d6e3b9 python3-twisted: Add python3-asyncio to RDEPENDS
    a5000c12a2 wireshark: fix CVE-2023-6175
    a9a4998947 python3-aiohttp: Fix CVE-2024-23334
    fdc54c5029 apache2: Upgrade v2.4.58 -> v2.4.59
    ca4f69e66c giflib: Fix CVE CVE-2022-28506
    ac06a65404 python3-django: fix CVE-2024-24680
    bd7b2ebf21 bats: fix bats-format-pretty report error when multilib
            enabled
    7a49f1e016 xterm: Security fix for CVE-2023-40359
    717462f811 python3-pillow: Fix CVE-2023-50447
    0fffd4d422 iniparser: Fix CVE-2023-33461
    c0fbf5751a wireshark: Backport fix for CVE-2024-2955
    a246e26a46 python3-pyyaml-include: Upgrade 1.3.2 -> 1.4.1
    b0e6f9c1de python3-pyyaml-include: switch to pytest --automake
    c944af7d57 python3-pyyaml-include: add initial recipe for version 1.3.2
    
    :Issues Addressed:
    [WRQ-18930] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=40fd5f4ee', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=b718257124', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=8a50230120', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=8bb1653353', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=5a6f7925bd', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
