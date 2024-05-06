1e2053c9c697ca375886191e9630d2f4e92ec8e2
Martin Jansa <martin2.jansa@lgepartner.com>
2024-05-06 13:12:01 +0200

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2024-05-10 (build 735 part 1/2)
    oe-core as of 2024-05-10 05:05:54 -0700
    
    f85d5dfc91 libpciaccess: Remove duplicated license entry
    10b57ae56e glibc: Update to latest on stable 2.35 branch
    ce2d6ba5d6 gstreamer1.0-plugins-bad: fix CVE-2023-50186
    adaebd54ea bluez5: Fix CVE-2023-27349 CVE-2023-50229 & CVE-2023-50230
    cc2d927520 xserver-xorg: fix CVE-2024-31083
    32fc43f0c3 xserver-xorg: fix CVE-2024-31082
    
    :Issues Addressed:
    [WRQ-21131] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=40fd5f4ee', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=24fd9b6756', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=f85d5dfc91', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=5a6f7925bd', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
