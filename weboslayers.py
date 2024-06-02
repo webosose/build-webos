60c8fb3cad133e21f9f0d50d2c955c1ebdb63b53
Martin Jansa <martin2.jansa@lgepartner.com>
2024-06-02 13:01:47 +0200

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2024-06-04 (build 748 part 1/2)
    oe-core as of 2024-06-01 19:12:27 -0700
    
    ab2649ef6c build-appliance-image: Update to kirkstone head revision
    209c41377a git: Fix multiple CVEs
    28d9f94853 util-linux: Fix CVE-2024-28085
    66228a9e81 ghostscript: fix CVE-2023-52722
    18e03cadca ghostscript: fix CVE-2024-29510
    edcaa55aa5 ghostscript: fix CVE-2024-33871
    fb0271a2d4 ghostscript: fix CVE-2024-33869
    9f0c63b568 ghostscript: fix CVE-2024-33870
    
    meta-oe as of 2024-06-02 15:16:32 -0400
    
    8e297cdc84 nginx: Remove obsolete patch
    3eb9002ce7 nodejs: fix CVE-2023-46809
    17db7e96c4 nodejs: fix CVE-2024-22025
    7b468c6f83 nodejs: fix CVE-2024-22019
    
    meta-raspberrypi as of 2024-06-04 20:49:35 +0100
    
    d7544f3 rpi-base: Removed hifiberry amp4pro.
    d2bbc1f rpi-base: Added missing HiFiBerry
    160ae17 rpi-base: Add missing hifiberry overlay
    
    :Issues Addressed:
    [WRQ-23181] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -60,9 +60,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=5a90927f3', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=e0a1ed7aa1', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=ab2649ef6c', 'oe-core'),
 
-('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=0560b84899', 'meta-oe'),
+('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=8e297cdc84', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-networking',           12, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 ('meta-python',               13, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -81,7 +81,7 @@ webos_layers = [
 
 ('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=de4a2aeb', ''),
 
-('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=70c293b', ''),
+('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=d7544f3', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
