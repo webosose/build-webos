00426701fe959300b338e03dc04b28c831971c98
ywbyun0815 <yw.byun@lge.com>
2024-06-05 19:12:46 +0900

    webos-image: Update layer pins for 2024-06-05 (build 730)
    meta-webosose as of 2024-06-05 18:45:34 +0900
    
    cc5c6425 webos: db8=31

--- weboslayers.py
+++ weboslayers.py
@@ -78,7 +78,7 @@ webos_layers = [
 ('meta-webos-backports-4.3',  32, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-backports-5.0',  33, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=0f81361a', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=cc5c6425', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=9dc6673', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
