0598517a73f4decd9042c92d716f5de73dbdf085
ywbyun0815 <yw.byun@lge.com>
2024-07-09 11:42:58 +0900

    webos-image: Update layer pins for 2024-07-09 (build 745)
    meta-webosose as of 2024-07-09 11:28:19 +0900
    
    a2cadbe2 webos: com.webos.service.mediarecorder=9-r5
             g-camera-pipeline=gav.60
    591c2f91 webos: com.webos.service.mediacontroller=33-r8
    748421ec webos: luna-surfacemanager=405
    98d27974 webos: gstreamer1.0-plugins-webosrs=rust.11

--- weboslayers.py
+++ weboslayers.py
@@ -79,7 +79,7 @@ webos_layers = [
 ('meta-webos-backports-5.0',  33, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-backports-5.1',  34, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=79abfc9e', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=a2cadbe2', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=70c293b', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
