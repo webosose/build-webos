3985546680acb383c3df2ea1737f5c08f0f539b0
ywbyun0815 <yw.byun@lge.com>
2024-07-09 11:36:06 +0900

    webos-image: Update layer pins for 2024-07-09 (build 739)
    meta-webosose as of 2024-07-09 11:27:14 +0900
    
    51734acf webos: com.webos.service.mediarecorder=8
             g-camera-pipeline=gav.59
    9b34532a webos: qtbase=webos122
    d735bea3 webos: qtwayland-webos=r21
    d8b7ddda webos: luna-surfacemanager=r62
    ae8f046c webos: auto-luna-surface-manager=r8
    8b4805f9 webos: webos-backports-5.0: skip rust-hello-world
    38c72f8e webos: webos-backports-5.0: rename rust-source and
             rust-snapshot to have PV suffix
    e2a73e4b webos: webos-backports-4.[12]: remove rust-1.63.0 backports
    b2717586 webos: sunpinyin=r4 (unpack files under ${S})
    655e30a9 webos: connman=webos25 wpa-supplicant=webos9 systemd=webos16
             telegraf=r4 node-red=r2 (use ${systemd_system_unitdir} instead
             of ${systemd_unitdir}/system)
    b68eaf49 webos: alsa-plugins=webos3 (remove useless duplicated
             do_install:append)
    3293d66b webos: replace.cmake: fix SOURCE/BINARY logic
    63360415 webos: replace.cmake: update cmake_minimum_required to 3.5.0
    b516e388 webos: edgeai-vision=plugin.16

--- weboslayers.py
+++ weboslayers.py
@@ -79,7 +79,7 @@ webos_layers = [
 ('meta-webos-backports-5.0',  33, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-backports-5.1',  34, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=e757199a', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=51734acf', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=9dc6673', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
