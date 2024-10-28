c6f5c955f8e9e25363f7539f84d729d751e2ca7c
Martin Jansa <martin2.jansa@lgepartner.com>
2024-10-28 09:25:07 +0100

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2024-11-02 (build 804 part 1/2)
    oe-core as of 2024-10-29 07:51:17 -0700
    
    2c913a7b66 util-linux: Define pidfd_* function signatures
    e361d9e102 at-spi2-core: backport a patch to fix build with gcc-14
            on host
    1054417a21 libffi: backport a fix to build libffi-native with gcc-14
    7cca344fea cracklib: Modify patch to compile with GCC 14
    0f850f2130 vala: add -Wno-error=incompatible-pointer-types work around
    fd31dd1abc zip: Fix build with gcc-14
    03b7a44e2f zip: Make configure checks to be more robust
    ed009b5d58 nativesdk-intercept: Fix bad intercept chgrp/chown logic
    7678ae7fc2 bmap-tools: update HOMEPAGE and SRC_URI
    302dd4a63f overlayfs-etc: add option to skip creation of mount dirs
    ec300eadd0 orc: upgrade 0.4.39 -> 0.4.40
    774fae9cb5 vim: Upgrade 9.1.0698 -> 9.1.0764
    45ef5c80b1 vim: Upgrade 9.1.0682 -> 9.1.0698
    8c20a7badb cve-check: add support for cvss v4.0
    3e47644d24 cve-check: add CVSS vector string to CVE database and
            reports
    2cf10084c5 python3: ignore fixed CVEs
    3782e1b218 qemu: fix CVE-2023-3019
    75e1dedf85 openssl: patch CVE-2024-9143
    c5a85dfe66 ghostscript: Backport CVE-2024-29508
    
    bitbake as of 2024-11-02 09:45:13 +0000
    
    0c30e9aad tests/fetch: Use our own mirror of mobile-broadband-provider
            to decouple from gnome gitlab
    e4ec4267e tests/fetch: Use our own mirror of sysprof to decouple from
            gnome gitlab
    15582daed gitsm: Remove downloads/tmpdir when failed
    ff2dfda55 gitsm: Add call_process_submodules() to remove duplicated
            code
    c092f7e6c bitbake: doc/user-manual: Update the BB_HASHSERVE_UPSTREAM
    53264dc14 fetch2/git: Use quote from shlex, not pipes
    
    meta-security as of 2024-09-15 10:58:59 -0400
    
    b9cf9cd clamav: Security fix for CVE-2024-20505 and CVE-2024-20506
    c50ef29 tpm2-tss: upgrade 3.2.2 -> 3.2.3
    bf1313a tpm2-tools: Upgrade 5.2 -> 5.7
    
    :Issues Addressed:
    [WRR-7925] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=eb5c1ce6b', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=0c30e9aad', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=983e3efb51', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=2c913a7b66', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=4ad41baed6', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -85,5 +85,5 @@ webos_layers = [
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-virtualization', 53, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
-('meta-security',             76, 'https://git.yoctoproject.org/git/meta-security',         'branch=kirkstone,commit=353078b', ''),
+('meta-security',             76, 'https://git.yoctoproject.org/git/meta-security',         'branch=kirkstone,commit=b9cf9cd', ''),
 ]
