e63a9bca8920a64e77048276d0a7389b5703abf3
ywbyun0815 <yw.byun@lge.com>
2024-10-28 11:42:15 +0900

    webos-image: Update layer pins for 2024-10-28 (build 791)
    meta-webosose as of 2024-10-26 23:27:01 +0900
    
    b336cad0 webos: enact-sandstone=r1

--- weboslayers.py
+++ weboslayers.py
@@ -79,7 +79,7 @@ webos_layers = [
 ('meta-webos-backports-5.0',  33, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-backports-5.1',  34, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=da30b10b', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=b336cad0', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=d7544f3', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
