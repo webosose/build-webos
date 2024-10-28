53d8f671d51ad2c4a9b1a4be5628d161e3995093
ywbyun0815 <yw.byun@lge.com>
2024-10-28 11:26:02 +0900

    webos-image: Update layer pins for 2024-10-28 (build 760)
    meta-webosose as of 2024-10-26 23:26:58 +0900
    
    b5a82d7b webos: com.webos.service.nettools=8-r2
    e4cb1c3e webos: com.webos.app.enactbrowser=17.browsershell.6
    f5eca92b webos: com.webos.app.enactbrowser=r22
    65b493f1 webos: wam=94 webruntime_120=7
    dd8d5ce8 webos: opkg=webos7
    f24e27df webos: recipes: lower buildpaths from ERROR_QA to WARN_QA
             for recipes which need it

--- weboslayers.py
+++ weboslayers.py
@@ -79,7 +79,7 @@ webos_layers = [
 ('meta-webos-backports-5.0',  33, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 ('meta-webos-backports-5.1',  34, 'https://github.com/webosose/meta-webosose.git',          '', ''),
 
-('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=8262ff3b', ''),
+('meta-webos',                40, 'https://github.com/webosose/meta-webosose.git',          'branch=master,commit=b5a82d7b', ''),
 
 ('meta-raspberrypi',          50, 'https://github.com/agherzan/meta-raspberrypi.git',       'branch=kirkstone,commit=d7544f3', ''),
 ('meta-webos-raspberrypi',    51, 'https://github.com/webosose/meta-webosose.git',          '', ''),
