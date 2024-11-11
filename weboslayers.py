d464327bc2e0bb925cbb7aec1a0885aed139e5d0
Martin Jansa <martin2.jansa@lgepartner.com>
2024-11-11 11:21:44 +0100

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2024-11-12 (build 809)
    oe-core as of 2024-11-15 12:18:46 -0800
    
    fb45c5cf8c build-appliance-image: Update to kirkstone head revision
    89b64526ac pseudo: Fix envp bug and add posix_spawn wrapper
    cc87b332c5 pseudo: Update to include open symlink handling bugfix
    63d7c11b46 pseudo: Fix to work with glibc 2.40
    8252670382 pseudo: Update to pull in python 3.12+ fix
    c254984403 pseudo: Update to pull in fchmodat fix
    1b181d1fce pseudo: Update to pull in gcc14 fix and missing statvfs64
            intercept
    40a013f713 pseudo: Update to pull in syncfs probe fix
    b8445072f4 pseudo: Update to pull in fd leak fix
    9b06d27411 pseudo: Update to include logic fix
    198ff9141f pseudo: Switch back to the master branch
    215367b62a pseudo: Disable LFS on 32bit arches
    77d018ce3b pseudo: Update to pull in linux-libc-headers race fix
    8e2233fd05 cmake: Fix sporadic issues when determining compiler
            internals
    8eec5c4417 patch.py: Use shlex instead of deprecated pipe
    2400e14347 glib-2.0: patch regression of CVE-2023-32665
    2cf8325876 expat: patch CVE-2024-50602
    e64d90d4c5 gstreamer1.0: ignore CVE-2024-0444
    fbb8928ea8 curl: patch CVE-2024-9681
    3e9018fb14 ghostscript: fix CVE-2023-46361
    2e8819c0b9 xmlto: backport a patch to fix build with gcc-14 on host
    80289f49d0 package: Switch debug source handling to use prefix map
    4c75edda8e gcc: restore a patch for Neoverse N2 core
    9b6363994e cve_check: Use a local copy of the database during builds
    eb9c981808 zstd: patch CVE-2022-4899
    
    bitbake as of 2024-11-02 06:30:04 -0700
    
    fb73c495c codeparser: Fix handling of string AST nodes with older
            Python versions
    
    meta-virtualization as of 2024-11-12 23:19:19 +0000
    
    c996df33 go-cli: use main branch
    
    :Issues Addressed:
    [WRR-9978] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -58,9 +58,9 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 # github.com/openembedded repositories are read-only mirrors of the authoritative
 # repositories on git.openembedded.org
 webos_layers = [
-('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=0c30e9aad', ''),
+('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=fb73c495c', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=2c913a7b66', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=fb45c5cf8c', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=4ad41baed6', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
@@ -69,7 +69,7 @@ webos_layers = [
 ('meta-filesystems',          14, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
 
 ('meta-clang',                15, 'https://github.com/kraj/meta-clang.git',                 'branch=kirkstone,commit=8bc893d', ''),
-('meta-virtualization',       17, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=02a6c00d', ''),
+('meta-virtualization',       17, 'https://git.yoctoproject.org/git/meta-virtualization',   'branch=kirkstone,commit=c996df33', ''),
 
 ('meta-qt6',                  20, 'https://code.qt.io/yocto/meta-qt6.git',                  'branch=6.8.0,commit=v6.8.0', ''),
 
