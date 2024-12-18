1150a59027b5fa4d192b72da21ba709b7d28f032
Martin Jansa <martin2.jansa@lgepartner.com>
2024-12-18 16:38:43 +0100

    Update to latest upstream revisions in Yocto 4.0 Kirkstone as of 2024-12-18 (build 823 part 1/2)
    oe-core as of 2024-12-18 07:06:28 -0800
    
    a20b02fdfe base-passwd: Add the sgx group
    2e251b4ebe base-passwd: fix patchreview warning
    4cafad1a0e base-passwd: add the wheel group
    f68617115d base-passwd: Update the status for two patches
    65f01b1e94 base-passwd: Update to 3.5.52
    1742f47e13 base-passwd: Regenerate the patches
    d167661bce package.bbclass: Use shlex instead of deprecated pipes
    2082038de0 subversion: fix CVE-2024-46901
    95027410db xserver-xorg: fix CVE-2024-9632
    c73af2d77f cve-update-nvd2-native: Tweak to work better with NFS DL_DIR
    
    :Issues Addressed:
    [WRR-14596] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.0,commit=3f88b0052', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=b132b817f5', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=kirkstone,commit=a20b02fdfe', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=kirkstone,commit=7b3fdcdfaa', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
