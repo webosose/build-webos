9889f1c01d14b8831673304f183a32687bc6d1db
Martin Jansa <martin2.jansa@lgepartner.com>
2025-01-27 10:00:12 +0100

    Update to latest upstream revisions in Yocto 5.0 Scarthgap as of 2025-01-29 (build 841 part 1/2)
    oe-core as of 2025-01-29 08:19:33 -0800
    
    37835788d07 uboot-config: fix devtool modify with kernel-fitimage
    f50306ea3e1 devtool: ide-sdk remove the plugin from eSDK installer
    1528d6aa060 oe-selftest: devtool ide-sdk use modify debug-build
    23795962683 devtool: ide-sdk recommend DEBUG_BUILD
    b12dbde1ea3 devtool: ide-sdk sort cmake preset
    fa30d8dd713 devtool: modify support debug-builds
    d7bd9c62766 u-boot: kernel-fitimage: Fix dependency loop if
                UBOOT_SIGN_ENABLE and UBOOT_ENV enabled
    35bf053cd41 go: upgrade 1.22.10 -> 1.22.11
    4d35279eed6 go: upgrade 1.22.9 -> 1.22.10
    a424422df97 go: upgrade 1.22.8 -> 1.22.9
    57ca5a2c912 systemd: upgrade 255.13 -> 255.17
    7f9bb493941 openssl: patch CVE-2024-13176
    62cb1296739 build-appliance-image: Update to scarthgap head revision
    9b23f995fbc rust: correctly link rust-snapshot into build/stage0
    d592bc02b08 rust: use rust-snapshot binaries only in rust-native
    4d739fe248d rust: build the default set of tools
    85d970756cd Revert "rust: Add new varaible RUST_ENABLE_EXTRA_TOOLS"
    
    :Issues Addressed:
    [WRR-18866] CCC: Various build fixes

--- weboslayers.py
+++ weboslayers.py
@@ -60,7 +60,7 @@ Machines = ['qemux86', 'qemux86-64', 'qemuarm', 'raspberrypi3', 'raspberrypi3-64
 webos_layers = [
 ('bitbake',                   -1, 'https://github.com/openembedded/bitbake.git',            'branch=2.8,commit=aa0e540fc31', ''),
 
-('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=3c31639dd53', 'oe-core'),
+('meta',                       5, 'https://github.com/openembedded/openembedded-core.git',  'branch=scarthgap,commit=37835788d07', 'oe-core'),
 
 ('meta-oe',                   10, 'https://github.com/openembedded/meta-openembedded.git',  'branch=scarthgap,commit=dda0d533260', 'meta-oe'),
 ('meta-multimedia',           11, 'https://github.com/openembedded/meta-openembedded.git',  '', 'meta-oe'),
